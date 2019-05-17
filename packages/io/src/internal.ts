47cd43c74480ad5ca5c94929edc1e27eb8d03fb2
Danny McCormick <damccorm@microsoft.com>
2019-05-17 16:22:59 -0400

    Add io lib

---
+++ packages/io/src/internal.ts
@@ -0,0 +1,147 @@
+import fs = require('fs')
+import path = require('path')
+
+export function _removeDirectory(directoryPath: string) {
+  if (fs.existsSync(directoryPath)) {
+    fs.readdirSync(directoryPath).forEach(fileName => {
+      let file = path.join(directoryPath, fileName)
+      if (fs.lstatSync(file).isDirectory()) {
+        _removeDirectory(file)
+      } else {
+        fs.unlinkSync(file)
+      }
+    })
+  }
+  fs.rmdirSync(directoryPath)
+}
+
+/**
+ * On OSX/Linux, true if path starts with '/'. On Windows, true for paths like:
+ * \, \hello, \\hello\share, C:, and C:\hello (and corresponding alternate separator cases).
+ */
+export function _isRooted(p: string): boolean {
+  p = _normalizeSeparators(p)
+  if (!p) {
+    throw new Error('isRooted() parameter "p" cannot be empty')
+  }
+
+  if (process.platform == 'win32') {
+    return (
+      p.slice(0, '\\'.length) == '\\' || /^[A-Z]:/i.test(p) // e.g. \ or \hello or \\hello
+    ) // e.g. C: or C:\hello
+  }
+
+  return p.slice(0, '/'.length) == '/'
+}
+
+/**
+ * Best effort attempt to determine whether a file exists and is executable.
+ * @param filePath    file path to check
+ * @param extensions  additional file extensions to try
+ * @return if file exists and is executable, returns the file path. otherwise empty string.
+ */
+export function _tryGetExecutablePath(
+  filePath: string,
+  extensions: string[]
+): string {
+  try {
+    // test file exists
+    let stats: fs.Stats = fs.statSync(filePath)
+    if (stats.isFile()) {
+      if (process.platform == 'win32') {
+        // on Windows, test for valid extension
+        let fileName = path.basename(filePath)
+        let dotIndex = fileName.lastIndexOf('.')
+        if (dotIndex >= 0) {
+          let upperExt = fileName.substr(dotIndex).toUpperCase()
+          if (extensions.some(validExt => validExt.toUpperCase() == upperExt)) {
+            return filePath
+          }
+        }
+      } else {
+        if (_isUnixExecutable(stats)) {
+          return filePath
+        }
+      }
+    }
+  } catch (err) {
+    if (err.code != 'ENOENT') {
+      console.log(
+        `Unexpected error attempting to determine if executable file exists '${filePath}': ${err}`
+      )
+    }
+  }
+
+  // try each extension
+  let originalFilePath = filePath
+  for (let extension of extensions) {
+    let found = false
+    let filePath = originalFilePath + extension
+    try {
+      let stats: fs.Stats = fs.statSync(filePath)
+      if (stats.isFile()) {
+        if (process.platform == 'win32') {
+          // preserve the case of the actual file (since an extension was appended)
+          try {
+            let directory = path.dirname(filePath)
+            let upperName = path.basename(filePath).toUpperCase()
+            for (let actualName of fs.readdirSync(directory)) {
+              if (upperName == actualName.toUpperCase()) {
+                filePath = path.join(directory, actualName)
+                break
+              }
+            }
+          } catch (err) {
+            console.log(
+              `Unexpected error attempting to determine the actual case of the file '${filePath}': ${err}`
+            )
+          }
+
+          return filePath
+        } else {
+          if (_isUnixExecutable(stats)) {
+            return filePath
+          }
+        }
+      }
+    } catch (err) {
+      if (err.code != 'ENOENT') {
+        console.log(
+          `Unexpected error attempting to determine if executable file exists '${filePath}': ${err}`
+        )
+      }
+    }
+  }
+
+  return ''
+}
+
+function _normalizeSeparators(p: string): string {
+  p = p || ''
+  if (process.platform == 'win32') {
+    // convert slashes on Windows
+    p = p.replace(/\//g, '\\')
+
+    // remove redundant slashes
+    let isUnc = /^\\\\+[^\\]/.test(p) // e.g. \\hello
+    return (isUnc ? '\\' : '') + p.replace(/\\\\+/g, '\\') // preserve leading // for UNC
+  }
+
+  // remove redundant slashes
+  return p.replace(/\/\/+/g, '/')
+}
+
+function _startsWith(str: string, start: string): boolean {
+  return str.slice(0, start.length) == start
+}
+
+// on Mac/Linux, test the execute bit
+//     R   W  X  R  W X R W X
+//   256 128 64 32 16 8 4 2 1
+function _isUnixExecutable(stats: fs.Stats) {
+  return (
+    (stats.mode & 1) > 0 ||
+    ((stats.mode & 8) > 0 && stats.gid === process.getgid()) ||
+    ((stats.mode & 64) > 0 && stats.uid === process.getuid())
+  )
+}
---
+++ packages/io/src/lib.ts
@@ -0,0 +1,401 @@
+import childProcess = require('child_process')
+import fs = require('fs')
+import path = require('path')
+import intm = require('./internal')
+import im = require('./interfaces')
+
+/**
+ * Copies a file or folder.
+ *
+ * @param     source    source path
+ * @param     dest      destination path
+ * @param     options   optional. See CopyOptions.
+ */
+export function cp(
+  source: string,
+  dest: string,
+  options?: im.CopyOptions
+): Promise<void> {
+  let force = true
+  let recursive = false
+  if (options) {
+    if (options.recursive) {
+      recursive = true
+    }
+    if (options.force === false) {
+      force = false
+    }
+  }
+  return new Promise<void>(async (resolve, reject) => {
+    try {
+      if (fs.lstatSync(source).isDirectory()) {
+        if (!recursive) {
+          throw new Error(`non-recursive cp failed, ${source} is a directory`)
+        }
+
+        // If directory exists, copy source inside it. Otherwise, create it and copy contents of source inside.
+        if (fs.existsSync(dest)) {
+          if (!fs.lstatSync(dest).isDirectory()) {
+            throw new Error(`${dest} is not a directory`)
+          }
+
+          dest = path.join(dest, path.basename(source))
+        }
+
+        copyDirectoryContents(source, dest, force)
+      } else {
+        if (force) {
+          fs.copyFileSync(source, dest)
+        } else {
+          fs.copyFileSync(source, dest, fs.constants.COPYFILE_EXCL)
+        }
+      }
+    } catch (err) {
+      reject(err)
+      return
+    }
+
+    resolve()
+  })
+}
+
+/**
+ * Moves a path.
+ *
+ * @param     source    source path
+ * @param     dest      destination path
+ * @param     options   optional. See CopyOptions.
+ */
+export function mv(
+  source: string,
+  dest: string,
+  options?: im.CopyOptions
+): Promise<void> {
+  let force = true
+  let recursive = false
+  if (options) {
+    if (options.recursive) {
+      recursive = true
+    }
+    if (options.force === false) {
+      force = false
+    }
+  }
+  return new Promise<void>(async (resolve, reject) => {
+    try {
+      if (fs.lstatSync(source).isDirectory()) {
+        if (!recursive) {
+          throw new Error(`non-recursive cp failed, ${source} is a directory`)
+        }
+
+        // If directory exists, move source inside it. Otherwise, create it and move contents of source inside.
+        if (fs.existsSync(dest)) {
+          if (!fs.lstatSync(dest).isDirectory()) {
+            throw new Error(`${dest} is not a directory`)
+          }
+
+          dest = path.join(dest, path.basename(source))
+        }
+
+        copyDirectoryContents(source, dest, force, true)
+      } else {
+        if (force) {
+          fs.copyFileSync(source, dest)
+        } else {
+          fs.copyFileSync(source, dest, fs.constants.COPYFILE_EXCL)
+        }
+
+        // Delete file after copying since this is mv.
+        fs.unlinkSync(source)
+      }
+    } catch (err) {
+      reject(err)
+      return
+    }
+
+    resolve()
+  })
+}
+
+/**
+ * Remove a path recursively with force
+ *
+ * @param     inputPath     path to remove
+ */
+export function rmRF(inputPath: string): Promise<void> {
+  return new Promise<void>(async (resolve, reject) => {
+    if (process.platform === 'win32') {
+      // Node doesn't provide a delete operation, only an unlink function. This means that if the file is being used by another
+      // program (e.g. antivirus), it won't be deleted. To address this, we shell out the work to rd/del.
+      try {
+        if (fs.statSync(inputPath).isDirectory()) {
+          childProcess.execSync(`rd /s /q "${inputPath}"`)
+        } else {
+          childProcess.execSync(`del /f /a "${inputPath}"`)
+        }
+      } catch (err) {
+        // if you try to delete a file that doesn't exist, desired result is achieved
+        // other errors are valid
+        if (err.code != 'ENOENT') {
+          reject(err)
+          return
+        }
+      }
+
+      // Shelling out fails to remove a symlink folder with missing source, this unlink catches that
+      try {
+        fs.unlinkSync(inputPath)
+      } catch (err) {
+        // if you try to delete a file that doesn't exist, desired result is achieved
+        // other errors are valid
+        if (err.code != 'ENOENT') {
+          reject(err)
+          return
+        }
+      }
+    } else {
+      // get the lstats in order to workaround a bug in shelljs@0.3.0 where symlinks
+      // with missing targets are not handled correctly by "rm('-rf', path)"
+      let isDirectory = false
+      try {
+        isDirectory = fs.lstatSync(inputPath).isDirectory()
+      } catch (err) {
+        // if you try to delete a file that doesn't exist, desired result is achieved
+        // other errors are valid
+        if (err.code == 'ENOENT') {
+          resolve()
+        } else {
+          reject(err)
+        }
+        return
+      }
+
+      if (isDirectory) {
+        intm._removeDirectory(inputPath)
+        resolve()
+        return
+      } else {
+        fs.unlinkSync(inputPath)
+      }
+    }
+
+    resolve()
+  })
+}
+
+/**
+ * Make a directory.  Creates the full path with folders in between
+ * Will throw if it fails
+ *
+ * @param     p       path to create
+ * @returns   Promise<void>
+ */
+export function mkdirP(p: string): Promise<void> {
+  return new Promise<void>(async (resolve, reject) => {
+    try {
+      if (!p) {
+        throw new Error('Parameter p is required')
+      }
+
+      // build a stack of directories to create
+      let stack: string[] = []
+      let testDir: string = p
+      while (true) {
+        // validate the loop is not out of control
+        if (stack.length >= (process.env['TEST_MKDIRP_FAILSAFE'] || 1000)) {
+          // let the framework throw
+          fs.mkdirSync(p)
+          resolve()
+          return
+        }
+
+        let stats: fs.Stats
+        try {
+          stats = fs.statSync(testDir)
+        } catch (err) {
+          if (err.code == 'ENOENT') {
+            // validate the directory is not the drive root
+            let parentDir = path.dirname(testDir)
+            if (testDir == parentDir) {
+              throw new Error(
+                `Unable to create directory '${p}'. Root directory does not exist: '${testDir}'`
+              )
+            }
+
+            // push the dir and test the parent
+            stack.push(testDir)
+            testDir = parentDir
+            continue
+          } else if (err.code == 'UNKNOWN') {
+            throw new Error(
+              `Unable to create directory '${p}'. Unable to verify the directory exists: '${testDir}'. If directory is a file share, please verify the share name is correct, the share is online, and the current process has permission to access the share.`
+            )
+          } else {
+            throw err
+          }
+        }
+
+        if (!stats.isDirectory()) {
+          throw new Error(
+            `Unable to create directory '${p}'. Conflicting file exists: '${testDir}'`
+          )
+        }
+
+        // testDir exists
+        break
+      }
+
+      // create each directory
+      while (stack.length) {
+        let dir = stack.pop()! // non-null because `stack.length` was truthy
+        fs.mkdirSync(dir)
+      }
+    } catch (err) {
+      reject(err)
+      return
+    }
+
+    resolve()
+  })
+}
+
+/**
+ * Returns path of a tool had the tool actually been invoked.  Resolves via paths.
+ * If you check and the tool does not exist, it will throw.
+ *
+ * @param     tool              name of the tool
+ * @param     check             whether to check if tool exists
+ * @returns   Promise<string>   path to tool
+ */
+export function which(tool: string, check?: boolean): Promise<string> {
+  return new Promise<string>(async (resolve, reject) => {
+    if (!tool) {
+      reject("parameter 'tool' is required")
+      return
+    }
+
+    // recursive when check=true
+    if (check) {
+      let result: string = await which(tool, false)
+      if (!result) {
+        if (process.platform == 'win32') {
+          reject(
+            `Unable to locate executable file: ${tool}. Please verify either the file path exists or the file can be found within a directory specified by the PATH environment variable. Also verify the file has a valid extension for an executable file.`
+          )
+        } else {
+          reject(
+            `Unable to locate executable file: ${tool}. Please verify either the file path exists or the file can be found within a directory specified by the PATH environment variable. Also check the file mode to verify the file is executable.`
+          )
+        }
+        return
+      }
+    }
+
+    try {
+      // build the list of extensions to try
+      let extensions: string[] = []
+      if (process.platform == 'win32' && process.env['PATHEXT']) {
+        for (let extension of process.env['PATHEXT']!.split(path.delimiter)) {
+          if (extension) {
+            extensions.push(extension)
+          }
+        }
+      }
+
+      // if it's rooted, return it if exists. otherwise return empty.
+      if (intm._isRooted(tool)) {
+        let filePath: string = intm._tryGetExecutablePath(tool, extensions)
+        if (filePath) {
+          resolve(filePath)
+          return
+        }
+
+        resolve('')
+        return
+      }
+
+      // if any path separators, return empty
+      if (
+        tool.indexOf('/') >= 0 ||
+        (process.platform == 'win32' && tool.indexOf('\\') >= 0)
+      ) {
+        resolve('')
+        return
+      }
+
+      // build the list of directories
+      //
+      // Note, technically "where" checks the current directory on Windows. From a task lib perspective,
+      // it feels like we should not do this. Checking the current directory seems like more of a use
+      // case of a shell, and the which() function exposed by the task lib should strive for consistency
+      // across platforms.
+      let directories: string[] = []
+      if (process.env['PATH']) {
+        for (let p of process.env['PATH']!.split(path.delimiter)) {
+          if (p) {
+            directories.push(p)
+          }
+        }
+      }
+
+      // return the first match
+      for (let directory of directories) {
+        let filePath = intm._tryGetExecutablePath(
+          directory + path.sep + tool,
+          extensions
+        )
+        if (filePath) {
+          resolve(filePath)
+          return
+        }
+      }
+
+      resolve('')
+    } catch (err) {
+      reject(`which failed with message ${err.message}`)
+    }
+  })
+}
+
+// Copies contents of source into dest, making any necessary folders along the way.
+// Deletes the original copy if deleteOriginal is true
+function copyDirectoryContents(
+  source: string,
+  dest: string,
+  force: boolean,
+  deleteOriginal = false
+) {
+  if (fs.lstatSync(source).isDirectory()) {
+    if (fs.existsSync(dest)) {
+      if (!fs.lstatSync(dest).isDirectory()) {
+        throw new Error(`${dest} is not a directory`)
+      }
+    } else {
+      mkdirP(dest)
+    }
+
+    // Copy all child files, and directories recursively
+    let sourceChildren: string[] = fs.readdirSync(source)
+    sourceChildren.forEach(newSource => {
+      let newDest = path.join(dest, path.basename(newSource))
+      copyDirectoryContents(
+        path.resolve(source, newSource),
+        newDest,
+        force,
+        deleteOriginal
+      )
+    })
+    if (deleteOriginal) {
+      fs.rmdirSync(source)
+    }
+  } else {
+    if (force) {
+      fs.copyFileSync(source, dest)
+    } else {
+      fs.copyFileSync(source, dest, fs.constants.COPYFILE_EXCL)
+    }
+    if (deleteOriginal) {
+      fs.unlinkSync(source)
+    }
+  }
+}
