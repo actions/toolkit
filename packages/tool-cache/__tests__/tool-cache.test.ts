20bb3255e4e61d295cc571874ea9e1e6cba67d2c
Danny McCormick <damccorm@microsoft.com>
2019-05-28 15:30:17 -0400

    Add tool-cache

---
+++ packages/tool-cache/__tests__/tool-cache.test.ts
@@ -0,0 +1,279 @@
+import * as fs from 'fs'
+import * as nock from 'nock'
+import * as path from 'path'
+import * as io from '@actions/io'
+import * as exec from '@actions/exec'
+import * as tc from '../src/tool-cache'
+
+'use strict'
+
+const cachePath = path.join(__dirname, 'CACHE')
+const tempPath = path.join(__dirname, 'TEMP')
+const IS_WINDOWS = process.platform === 'win32'
+
+describe('@actions/toolCache', function() {
+  beforeAll(function() {
+    nock('http://microsoft.com')
+      .persist()
+      .get('/bytes/35')
+      .reply(200, {
+        username: 'abc',
+        password: 'def'
+      })
+  })
+
+  beforeEach(async function() {
+    await io.rmRF(cachePath)
+    await io.rmRF(tempPath)
+    await io.mkdirP(cachePath)
+    await io.mkdirP(tempPath)
+    process.env['Runner.TempDirectory'] = tempPath
+    process.env['Runner.ToolsDirectory'] = cachePath
+  })
+
+  afterAll(async function() {
+    await io.rmRF(tempPath)
+    await io.rmRF(cachePath)
+  })
+
+  it('downloads a 35 byte file', async () => {
+    const downPath: string = await tc.downloadTool(
+      'http://microsoft.com/bytes/35'
+    )
+
+    expect(fs.existsSync(downPath)).toBeTruthy()
+    expect(fs.statSync(downPath).size).toBe(35)
+  })
+
+  it('downloads a 35 byte file after a redirect', async () => {
+    nock('http://microsoft.com')
+      .get('/redirect-to')
+      .reply(303, undefined, {
+        location: 'http://microsoft.com/bytes/35'
+      })
+
+    const downPath: string = await tc.downloadTool(
+      'http://microsoft.com/redirect-to'
+    )
+
+    expect(fs.existsSync(downPath)).toBeTruthy()
+    expect(fs.statSync(downPath).size).toBe(35)
+  })
+
+  it('has status code in exception dictionary for HTTP error code responses', async () => {
+    nock('http://microsoft.com')
+      .get('/bytes/bad')
+      .reply(400, {
+        username: 'bad',
+        password: 'file'
+      })
+
+    try {
+      const errorCodeUrl = 'http://microsoft.com/bytes/bad'
+      await tc.downloadTool(errorCodeUrl)
+      throw new Error('Should have failed')
+    } catch (err) {
+      expect(err.toString()).toContain('Unexpected HTTP response: 400')
+    }
+  })
+
+  it('works with redirect code 302', async function() {
+    nock('http://microsoft.com')
+      .get('/redirect-to')
+      .reply(302, undefined, {
+        location: 'http://microsoft.com/bytes/35'
+      })
+
+    const downPath: string = await tc.downloadTool(
+      'http://microsoft.com/redirect-to'
+    )
+
+    expect(fs.existsSync(downPath)).toBeTruthy()
+    expect(fs.statSync(downPath).size).toBe(35)
+  })
+
+  it('installs a binary tool and finds it', async () => {
+    const downPath: string = await tc.downloadTool(
+      'http://microsoft.com/bytes/35'
+    )
+
+    expect(fs.existsSync(downPath)).toBeTruthy()
+
+    await tc.cacheFile(downPath, 'foo', 'foo', '1.1.0')
+
+    const toolPath: string = tc.find('foo', '1.1.0')
+    expect(fs.existsSync(toolPath)).toBeTruthy()
+    expect(fs.existsSync(`${toolPath}.complete`)).toBeTruthy()
+
+    const binaryPath: string = path.join(toolPath, 'foo')
+    expect(fs.existsSync(binaryPath)).toBeTruthy()
+  })
+
+  if (IS_WINDOWS) {
+    it('installs a 7z and finds it', async () => {
+      const tempDir = path.join(__dirname, 'test-install-7z')
+      try {
+        await io.mkdirP(tempDir)
+
+        // copy the 7z file to the test dir
+        const _7zFile: string = path.join(tempDir, 'test.7z')
+        await io.cp(path.join(__dirname, 'data', 'test.7z'), _7zFile)
+
+        // extract/cache
+        const extPath: string = await tc.extract7z(_7zFile)
+        await tc.cacheDir(extPath, 'my-7z-contents', '1.1.0')
+        const toolPath: string = tc.find('my-7z-contents', '1.1.0')
+
+        expect(fs.existsSync(toolPath)).toBeTruthy()
+        expect(fs.existsSync(`${toolPath}.complete`)).toBeTruthy()
+        expect(fs.existsSync(path.join(toolPath, 'file.txt'))).toBeTruthy()
+        expect(
+          fs.existsSync(path.join(toolPath, 'file-with-ç-character.txt'))
+        ).toBeTruthy()
+        expect(
+          fs.existsSync(path.join(toolPath, 'folder', 'nested-file.txt'))
+        ).toBeTruthy()
+      } finally {
+        await io.rmRF(tempDir)
+      }
+    })
+  }
+
+  it('installs a zip and finds it', async () => {
+    const tempDir = path.join(__dirname, 'test-install-zip')
+    try {
+      await io.mkdirP(tempDir)
+
+      // stage the layout for a zip file:
+      //   file.txt
+      //   folder/nested-file.txt
+      const stagingDir = path.join(tempDir, 'zip-staging')
+      await io.mkdirP(path.join(stagingDir, 'folder'))
+      fs.writeFileSync(path.join(stagingDir, 'file.txt'), '')
+      fs.writeFileSync(path.join(stagingDir, 'folder', 'nested-file.txt'), '')
+
+      // create the zip
+      const zipFile = path.join(tempDir, 'test.zip')
+      await io.rmRF(zipFile)
+      if (IS_WINDOWS) {
+        const escapedStagingPath = stagingDir.replace(/'/g, "''") // double-up single quotes
+        const escapedZipFile = zipFile.replace(/'/g, "''")
+        const powershellPath = await io.which('powershell', true)
+        const args = [
+          '-NoLogo',
+          '-Sta',
+          '-NoProfile',
+          '-NonInteractive',
+          '-ExecutionPolicy',
+          'Unrestricted',
+          '-Command',
+          `$ErrorActionPreference = 'Stop' ; Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::CreateFromDirectory('${escapedStagingPath}', '${escapedZipFile}')`
+        ]
+        await exec.exec(`"${powershellPath}"`, args)
+      } else {
+        const zipPath: string = await io.which('zip', true)
+        await exec.exec(`"${zipPath}`, [zipFile, '-r', '.'], {cwd: stagingDir})
+      }
+
+      const extPath: string = await tc.extractZip(zipFile)
+      await tc.cacheDir(extPath, 'foo', '1.1.0')
+      const toolPath: string = tc.find('foo', '1.1.0')
+
+      expect(fs.existsSync(toolPath)).toBeTruthy()
+      expect(fs.existsSync(`${toolPath}.complete`)).toBeTruthy()
+      expect(fs.existsSync(path.join(toolPath, 'file.txt'))).toBeTruthy()
+      expect(
+        fs.existsSync(path.join(toolPath, 'folder', 'nested-file.txt'))
+      ).toBeTruthy()
+    } finally {
+      await io.rmRF(tempDir)
+    }
+  })
+
+  it('installs a zip and extracts it to specified directory', async function() {
+    const tempDir = path.join(__dirname, 'test-install-zip')
+    try {
+    await io.mkdirP(tempDir)
+
+    // stage the layout for a zip file:
+    //   file.txt
+    //   folder/nested-file.txt
+    const stagingDir = path.join(tempDir, 'zip-staging')
+    await io.mkdirP(path.join(stagingDir, 'folder'))
+    fs.writeFileSync(path.join(stagingDir, 'file.txt'), '')
+    fs.writeFileSync(path.join(stagingDir, 'folder', 'nested-file.txt'), '')
+
+    // create the zip
+    const zipFile = path.join(tempDir, 'test.zip')
+    await io.rmRF(zipFile)
+    if (IS_WINDOWS) {
+      const escapedStagingPath = stagingDir.replace(/'/g, "''") // double-up single quotes
+      const escapedZipFile = zipFile.replace(/'/g, "''")
+      const powershellPath = await io.which('powershell', true)
+      const args = [
+        '-NoLogo',
+        '-Sta',
+        '-NoProfile',
+        '-NonInteractive',
+        '-ExecutionPolicy',
+        'Unrestricted',
+        '-Command',
+        `$ErrorActionPreference = 'Stop' ; Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::CreateFromDirectory('${escapedStagingPath}', '${escapedZipFile}')`
+      ]
+      await exec.exec(`"${powershellPath}"`, args)
+    } else {
+      const zipPath = await io.which('zip')
+      await exec.exec(zipPath, [zipFile, '-r', '.'], {cwd: stagingDir})
+    }
+
+    const destDir = path.join(__dirname, 'unzip-dest')
+    await io.rmRF(destDir)
+    await io.mkdirP(destDir)
+    try {
+      const extPath: string = await tc.extractZip(zipFile, destDir)
+      await tc.cacheDir(extPath, 'foo', '1.1.0')
+      const toolPath: string = tc.find('foo', '1.1.0')
+      expect(fs.existsSync(toolPath)).toBeTruthy()
+      expect(fs.existsSync(`${toolPath}.complete`)).toBeTruthy()
+      expect(fs.existsSync(path.join(toolPath, 'file.txt'))).toBeTruthy()
+      expect(
+        fs.existsSync(path.join(toolPath, 'folder', 'nested-file.txt'))
+      ).toBeTruthy()
+    } finally {
+      await io.rmRF(destDir)
+    }
+    }
+    finally {
+      await io.rmRF(tempDir)
+    }
+  })
+
+  it('works with a 502 temporary failure', async function() {
+    nock('http://microsoft.com')
+      .get('/temp502')
+      .twice()
+      .reply(502, undefined)
+    nock('http://microsoft.com')
+      .get('/temp502')
+      .reply(200, undefined)
+
+    const statusCodeUrl = 'http://microsoft.com/temp502'
+    await tc.downloadTool(statusCodeUrl)
+  })
+
+  it("doesn't retry 502s more than 3 times", async function() {
+    nock('http://microsoft.com')
+      .get('/perm502')
+      .times(3)
+      .reply(502, undefined)
+
+    let thrown = false
+    try {
+      const statusCodeUrl = 'http://microsoft.com/perm502'
+      await tc.downloadTool(statusCodeUrl)
+    } catch (err) {
+      thrown = true
+    }
+    expect(thrown).toBeTruthy()
+  })
+})
---
+++ packages/tool-cache/src/tool-cache.ts
@@ -0,0 +1,438 @@
+import * as core from '@actions/core'
+import * as io from '@actions/io'
+import * as fs from 'fs'
+import * as os from 'os'
+import * as path from 'path'
+import * as httpm from 'typed-rest-client/HttpClient'
+import * as semver from 'semver'
+import * as uuidV4 from 'uuid/v4'
+import {exec} from '@actions/exec/lib/exec'
+import {ExecOptions} from '@actions/exec/lib/interfaces'
+
+const IS_WINDOWS = process.platform === 'win32'
+const userAgent = 'actions/tool-cache'
+
+/**
+ * Download a tool from an url and stream it into a file
+ *
+ * @param url       url of tool to download
+ * @returns         path to downloaded tool
+ */
+export async function downloadTool(url: string): Promise<string> {
+  return new Promise<string>(async (resolve, reject) => {
+    try {
+      const http: httpm.HttpClient = new httpm.HttpClient(userAgent, [], {
+        allowRetries: true,
+        maxRetries: 3
+      })
+      const destPath: string = path.join(_getAgentTemp(), uuidV4())
+
+      await io.mkdirP(_getAgentTemp())
+      core.debug(`Downloading ${url}`)
+      core.debug(`Downloading ${destPath}`)
+
+      if (fs.existsSync(destPath)) {
+        throw new Error(`Destination file path ${destPath} already exists`)
+      }
+
+      const response: httpm.HttpClientResponse = await http.get(url)
+
+      if (response.message.statusCode !== 200) {
+        const err: Error = new Error(
+          `Unexpected HTTP response: ${response.message.statusCode}`
+        )
+        core.debug(
+          `Failed to download from "${url}". Code(${
+            response.message.statusCode
+          }) Message(${response.message.statusMessage})`
+        )
+        throw err
+      }
+
+      const file: NodeJS.WritableStream = fs.createWriteStream(destPath)
+      file.on('open', async () => {
+        try {
+          const stream = response.message.pipe(file)
+          stream.on('close', () => {
+            core.debug('download complete')
+            resolve(destPath)
+          })
+        } catch (err) {
+          core.debug(
+            `Failed to download from "${url}". Code(${
+              response.message.statusCode
+            }) Message(${response.message.statusMessage})`
+          )
+          reject(err)
+        }
+      })
+      file.on('error', err => {
+        file.end()
+        reject(err)
+      })
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
+/**
+ * Extract a .7z file
+ *
+ * @param file     path to the .7z file
+ * @param dest     destination directory. Optional.
+ * @returns        path to the destination directory
+ */
+export async function extract7z(file: string, dest?: string): Promise<string> {
+  if (!IS_WINDOWS) {
+    throw new Error('extract7z() not supported on current OS')
+  }
+
+  if (!file) {
+    throw new Error("parameter 'file' is required")
+  }
+
+  dest = dest || (await _createExtractFolder(dest))
+
+  const originalCwd = process.cwd()
+  try {
+    process.chdir(dest)
+    const escapedScript = path
+      .join(__dirname, 'Invoke-7zdec.ps1')
+      .replace(/'/g, "''")
+      .replace(/"|\n|\r/g, '') // double-up single quotes, remove double quotes and newlines
+    const escapedFile = file.replace(/'/g, "''").replace(/"|\n|\r/g, '')
+    const escapedTarget = dest.replace(/'/g, "''").replace(/"|\n|\r/g, '')
+    const command = `& '${escapedScript}' -Source '${escapedFile}' -Target '${escapedTarget}'`
+    const powershellPath: string = await io.which('powershell', true)
+    const args: string[] = [
+      '-NoLogo',
+      '-Sta',
+      '-NoProfile',
+      '-NonInteractive',
+      '-ExecutionPolicy',
+      'Unrestricted',
+      '-Command',
+      command
+    ]
+    const options: ExecOptions = {
+      silent: true,
+      listeners: {
+        stdout: (data: Buffer) => {
+          process.stdout.write(data)
+        },
+        stderr: (data: Buffer) => {
+          process.stderr.write(data)
+        }
+      }
+    }
+    await exec(`"${powershellPath}"`, args, options)
+  } finally {
+    process.chdir(originalCwd)
+  }
+
+  return dest
+}
+
+/**
+ * Extract a tar
+ *
+ * @param file     path to the tar
+ * @param dest     destination directory. Optional.
+ * @returns        path to the destination directory
+ */
+export async function extractTar(file: string, dest?: string): Promise<string> {
+  if (!file) {
+    throw new Error("parameter 'file' is required")
+  }
+
+  dest = dest || (await _createExtractFolder(dest))
+  const tarPath: string = await io.which('tar', true)
+  await exec(`"${tarPath}"`, ['xzC', dest, '-f', file])
+
+  return dest
+}
+
+/**
+ * Extract a zip
+ *
+ * @param file     path to the zip
+ * @param dest     destination directory. Optional.
+ * @returns        path to the destination directory
+ */
+export async function extractZip(file: string, dest?: string): Promise<string> {
+  if (!file) {
+    throw new Error("parameter 'file' is required")
+  }
+
+  dest = dest || (await _createExtractFolder(dest))
+
+  if (IS_WINDOWS) {
+    // build the powershell command
+    const escapedFile = file.replace(/'/g, "''").replace(/"|\n|\r/g, '') // double-up single quotes, remove double quotes and newlines
+    const escapedDest = dest.replace(/'/g, "''").replace(/"|\n|\r/g, '')
+    const command = `$ErrorActionPreference = 'Stop' ; try { Add-Type -AssemblyName System.IO.Compression.FileSystem } catch { } ; [System.IO.Compression.ZipFile]::ExtractToDirectory('${escapedFile}', '${escapedDest}')`
+
+    // run powershell
+    const powershellPath = await io.which('powershell')
+    const args = [
+      '-NoLogo',
+      '-Sta',
+      '-NoProfile',
+      '-NonInteractive',
+      '-ExecutionPolicy',
+      'Unrestricted',
+      '-Command',
+      command
+    ]
+    await exec(`"${powershellPath}"`, args)
+  } else {
+    const unzipPath = await io.which('powershell')
+    await exec(`"${unzipPath}"`, [file], {cwd: dest})
+  }
+
+  return dest
+}
+
+/**
+ * Caches a directory and installs it into the tool cacheDir
+ *
+ * @param sourceDir    the directory to cache into tools
+ * @param tool          tool name
+ * @param version       version of the tool.  semver format
+ * @param arch          architecture of the tool.  Optional.  Defaults to machine architecture
+ */
+export async function cacheDir(
+  sourceDir: string,
+  tool: string,
+  version: string,
+  arch?: string
+): Promise<string> {
+  version = semver.clean(version) || version
+  arch = arch || os.arch()
+  core.debug(`Caching tool ${tool} ${version} ${arch}`)
+
+  core.debug(`source dir: ${sourceDir}`)
+  if (!fs.statSync(sourceDir).isDirectory()) {
+    throw new Error('sourceDir is not a directory')
+  }
+
+  // Create the tool dir
+  const destPath: string = await _createToolPath(tool, version, arch)
+  // copy each child item. do not move. move can fail on Windows
+  // due to anti-virus software having an open handle on a file.
+  for (const itemName of fs.readdirSync(sourceDir)) {
+    const s = path.join(sourceDir, itemName)
+    await io.cp(s, destPath, {recursive: true})
+  }
+
+  // write .complete
+  _completeToolPath(tool, version, arch)
+
+  return destPath
+}
+
+/**
+ * Caches a downloaded file (GUID) and installs it
+ * into the tool cache with a given targetName
+ *
+ * @param sourceFile    the file to cache into tools.  Typically a result of downloadTool which is a guid.
+ * @param targetFile    the name of the file name in the tools directory
+ * @param tool          tool name
+ * @param version       version of the tool.  semver format
+ * @param arch          architecture of the tool.  Optional.  Defaults to machine architecture
+ */
+export async function cacheFile(
+  sourceFile: string,
+  targetFile: string,
+  tool: string,
+  version: string,
+  arch?: string
+): Promise<string> {
+  version = semver.clean(version) || version
+  arch = arch || os.arch()
+  core.debug(`Caching tool ${tool} ${version} ${arch}`)
+
+  core.debug(`source file: ${sourceFile}`)
+  if (!fs.statSync(sourceFile).isFile()) {
+    throw new Error('sourceFile is not a file')
+  }
+
+  // create the tool dir
+  const destFolder: string = await _createToolPath(tool, version, arch)
+
+  // copy instead of move. move can fail on Windows due to
+  // anti-virus software having an open handle on a file.
+  const destPath: string = path.join(destFolder, targetFile)
+  core.debug(`destination file ${destPath}`)
+  await io.cp(sourceFile, destPath)
+
+  // write .complete
+  _completeToolPath(tool, version, arch)
+
+  return destFolder
+}
+
+/**
+ * finds the path to a tool in the local installed tool cache
+ *
+ * @param toolName      name of the tool
+ * @param versionSpec   version of the tool
+ * @param arch          optional arch.  defaults to arch of computer
+ */
+export function find(
+  toolName: string,
+  versionSpec: string,
+  arch?: string
+): string {
+  if (!toolName) {
+    throw new Error('toolName parameter is required')
+  }
+
+  if (!versionSpec) {
+    throw new Error('versionSpec parameter is required')
+  }
+
+  arch = arch || os.arch()
+
+  // attempt to resolve an explicit version
+  if (!_isExplicitVersion(versionSpec)) {
+    const localVersions: string[] = _findLocalToolVersions(toolName, arch)
+    const match = _evaluateVersions(localVersions, versionSpec)
+    versionSpec = match
+  }
+
+  // check for the explicit version in the cache
+  let toolPath = ''
+  if (versionSpec) {
+    versionSpec = semver.clean(versionSpec) || ''
+    const cacheRoot = _getCacheRoot()
+    const cachePath = path.join(cacheRoot, toolName, versionSpec, arch)
+    core.debug(`checking cache: ${cachePath}`)
+    if (fs.existsSync(cachePath) && fs.existsSync(`${cachePath}.complete`)) {
+      core.debug(`Found tool in cache ${toolName} ${versionSpec} ${arch}`)
+      toolPath = cachePath
+    } else {
+      core.debug('not found')
+    }
+  }
+  return toolPath
+}
+
+async function _createExtractFolder(dest?: string): Promise<string> {
+  if (!dest) {
+    // create a temp dir
+    dest = path.join(_getAgentTemp(), uuidV4())
+  }
+  await io.mkdirP(dest)
+  return dest
+}
+
+function _getAgentTemp(): string {
+  // TODO - we need an actual protocol for this (this is just a placeholder)
+  const tempDirectory = process.env['Runner.TempDirectory']
+  if (!tempDirectory) {
+    throw new Error('Runner.TempDirectory is not set')
+  }
+
+  return tempDirectory
+}
+
+async function _createToolPath(
+  tool: string,
+  version: string,
+  arch?: string
+): Promise<string> {
+  const folderPath = path.join(
+    _getCacheRoot(),
+    tool,
+    semver.clean(version) || version,
+    arch || ''
+  )
+  core.debug(`destination ${folderPath}`)
+  const markerPath = `${folderPath}.complete`
+  await io.rmRF(folderPath)
+  await io.rmRF(markerPath)
+  await io.mkdirP(folderPath)
+  return folderPath
+}
+
+function _completeToolPath(tool: string, version: string, arch?: string): void {
+  const folderPath = path.join(
+    _getCacheRoot(),
+    tool,
+    semver.clean(version) || version,
+    arch || ''
+  )
+  const markerPath = `${folderPath}.complete`
+  fs.writeFileSync(markerPath, '')
+  core.debug('finished caching tool')
+}
+
+function _getCacheRoot(): string {
+  // TODO - we need an actual protocol for this (this is just a placeholder)
+  const cacheRoot = process.env['Runner.ToolsDirectory']
+  if (!cacheRoot) {
+    throw new Error('Runner.ToolsDirectory is not set')
+  }
+
+  return cacheRoot
+}
+
+function _isExplicitVersion(versionSpec: string): boolean {
+  const c = semver.clean(versionSpec) || ''
+  core.debug(`isExplicit: ${c}`)
+
+  const valid = semver.valid(c) != null
+  core.debug(`explicit? ${valid}`)
+
+  return valid
+}
+
+function _evaluateVersions(versions: string[], versionSpec: string): string {
+  let version = ''
+  core.debug(`evaluating ${versions.length} versions`)
+  versions = versions.sort((a, b) => {
+    if (semver.gt(a, b)) {
+      return 1
+    }
+    return -1
+  })
+  for (let i = versions.length - 1; i >= 0; i--) {
+    const potential: string = versions[i]
+    const satisfied: boolean = semver.satisfies(potential, versionSpec)
+    if (satisfied) {
+      version = potential
+      break
+    }
+  }
+
+  if (version) {
+    core.debug(`matched: ${version}`)
+  } else {
+    core.debug('match not found')
+  }
+
+  return version
+}
+
+function _findLocalToolVersions(toolName: string, arch?: string): string[] {
+  const versions: string[] = []
+
+  arch = arch || os.arch()
+  const toolPath = path.join(_getCacheRoot(), toolName)
+
+  if (fs.existsSync(toolPath)) {
+    const children: string[] = fs.readdirSync(toolPath)
+    for (const child of children) {
+      if (_isExplicitVersion(child)) {
+        const fullPath = path.join(toolPath, child, arch || '')
+        if (fs.existsSync(fullPath) && fs.existsSync(`${fullPath}.complete`)) {
+          versions.push(child)
+        }
+      }
+    }
+  }
+
+  return versions
+}
