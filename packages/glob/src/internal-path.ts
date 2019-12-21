857eb29e63fe7de6123fbd4a24ee61050514581e
eric sciple <ericsciple@users.noreply.github.com>
2019-12-20 23:08:59 -0500

    .

--- packages/glob/src/internal-path.ts
+++ packages/glob/src/internal-path.ts
@@ -5,46 +5,44 @@ import * as pathHelper from './internal-path-helper'
 const IS_WINDOWS = process.platform === 'win32'
 
 /**
- * Helper class for parsing paths into segments. The first separator is always
- * the root. On Windows, any slashes in the root segment are converted to `/`.
+ * Helper class for parsing paths into segments. On Windows, any slashes
+ * in the root segment are converted to `/`.
  */
 export class Path {
   segments: string[] = []
 
-  constructor(rootedPath: string) {
-    assert(rootedPath, `Parameter 'rootedPath' cannot be empty`)
-    assert(
-      pathHelper.isRooted(rootedPath),
-      `Parameter 'rootedPath' must be rooted. Invalid path: '${rootedPath}'`
-    )
+  constructor(itemPath: string) {
+    assert(itemPath, `Parameter 'itemPath' cannot be empty`)
 
     // Normalize slashes
     // Trim trailing slash
-    rootedPath = pathHelper.normalizeSeparators(rootedPath)
-    rootedPath = pathHelper.safeTrimTrailingSeparator(rootedPath)
+    itemPath = pathHelper.normalizeSeparators(itemPath)
+    itemPath = pathHelper.safeTrimTrailingSeparator(itemPath)
 
-    // Add all segments, while not at the root
-    let remaining = rootedPath
-    let dir = pathHelper.dirname(remaining)
-    while (dir !== remaining) {
-      // Add the segment
-      const basename = path.basename(remaining)
-      assert(basename !== '.', `Unexpected segment '.' in path '${rootedPath}'`)
-      assert(
-        basename !== '..',
-        `Unexpected segment '..' in path '${rootedPath}'`
-      )
-      this.segments.unshift(basename)
-
-      // Truncate the last segment
-      remaining = dir
-      dir = pathHelper.dirname(remaining)
+    // Not rooted
+    if (!pathHelper.isRooted(itemPath)) {
+      this.segments = itemPath.split(path.sep)
     }
+    // Rooted
+    else {
+      // Add all segments, while not at the root
+      let remaining = itemPath
+      let dir = pathHelper.dirname(remaining)
+      while (dir !== remaining) {
+        // Add the segment
+        const basename = path.basename(remaining)
+        this.segments.unshift(basename)
 
-    // Remainder is the root
-    // On Windows, convert any slashes in the root to '/'
-    this.segments.unshift(
-      IS_WINDOWS ? remaining.replace(/\\/g, '/') : remaining
-    )
+        // Truncate the last segment
+        remaining = dir
+        dir = pathHelper.dirname(remaining)
+      }
+
+      // Remainder is the root
+      // On Windows, convert any slashes in the root to '/'
+      this.segments.unshift(
+        IS_WINDOWS ? remaining.replace(/\\/g, '/') : remaining
+      )
+    }
   }
 }
