842a81a84f8cc031849f32cb66b2daaaadd096ed
eric sciple <ericsciple@users.noreply.github.com>
2020-01-06 14:21:22 -0500

    .

--- packages/glob/src/glob.ts
+++ packages/glob/src/glob.ts
@@ -1,27 +1,13 @@
-import * as core from '@actions/core'
 import {Globber, DefaultGlobber} from './internal-globber'
 import {GlobOptions} from './internal-glob-options'
 
 export {Globber, GlobOptions}
 
 /**
- * Contructs a globber from an input
- *
- * @param name  Name of the input
- */
-export async function getInput(
-  name: string,
-  options?: core.InputOptions
-): Promise<Globber> {
-  const input = core.getInput(name, options)
-  return await DefaultGlobber.parse(input)
-}
-
-/**
  * Constructs a globber
  *
  * @param patterns  Patterns separated by newlines
  */
-export async function parse(patterns: string): Promise<Globber> {
+export async function create(patterns: string): Promise<Globber> {
   return await DefaultGlobber.parse(patterns)
 }
