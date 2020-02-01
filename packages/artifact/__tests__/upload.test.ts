31ff9e6173bf79e942e931ffa0fd37e9feda3d70
konradpabjan <konradpabjan@outlook.com>
2020-01-31 22:30:18 -0500

    Fixes error with unbound methods being used in tests

--- packages/artifact/__tests__/upload.test.ts
+++ packages/artifact/__tests__/upload.test.ts
@@ -25,16 +25,13 @@ let file3Size = 0
 let file4Size = 0
 let file5Size = 0
 
-// mock env variables that will not always be available along with certain http methods
 jest.mock('../src/config-variables')
 jest.mock('@actions/http-client')
 
 describe('Upload Tests', () => {
-  // setup mocking for HTTP calls and prepare some test files for mock uploading
   beforeAll(async () => {
-    mockHttpPost()
-    mockHttpSendStream()
-    mockHttpPatch()
+    // setup mocking for calls that got through the HttpClient
+    setupHttpClientMock()
 
     // clear temp directory and create files that will be "uploaded"
     await io.rmRF(root)
@@ -67,6 +64,7 @@ describe('Upload Tests', () => {
   })
 
   afterAll(async () => {
+    jest.resetAllMocks()
     await io.rmRF(root)
   })
 
@@ -258,22 +256,28 @@ describe('Upload Tests', () => {
   })
 
   /**
-   * Helpers used to setup mocking all the required HTTP calls
+   * Helpers used to setup mocking for the HttpClient
    */
-  async function mockReadBodyEmpty(): Promise<string> {
+  async function emptyMockReadBody(): Promise<string> {
     return new Promise(resolve => {
       resolve()
     })
   }
 
-  function mockHttpPost(): void {
-    // eslint-disable-next-line @typescript-eslint/unbound-method
-    HttpClient.prototype.post = jest.fn(
-      async (requestdata, data): Promise<HttpClientResponse> => {
+  function setupHttpClientMock(): void {
+    /**
+     * Mocks Post calls that are used during Artifact Creation tests
+     *
+     * Simulates success and non-success status codes depending on the artifact name along with an appropriate
+     * payload that represents an expected response
+     */
+    jest
+      .spyOn(HttpClient.prototype, 'post')
+      .mockImplementation(async (requestdata, data) => {
         // parse the input data and use the provided artifact name as part of the response
         const inputData = JSON.parse(data)
         const mockMessage = new http.IncomingMessage(new net.Socket())
-        let mockReadBody = mockReadBodyEmpty
+        let mockReadBody = emptyMockReadBody
 
         if (inputData.Name === 'invalid-artifact-name') {
           mockMessage.statusCode = 400
@@ -303,14 +307,17 @@ describe('Upload Tests', () => {
             readBody: mockReadBody
           })
         })
-      }
-    )
-  }
+      })
 
-  function mockHttpSendStream(): void {
-    // eslint-disable-next-line @typescript-eslint/unbound-method
-    HttpClient.prototype.sendStream = jest.fn(
-      async (verb, requestUrl, stream) => {
+    /**
+     * Mocks SendStream calls that are made during Artifact Upload tests
+     *
+     * A 500 response is used to simulate a failed upload stream. The uploadUrl can be set to
+     * include 'fail' to specify that the upload should fail
+     */
+    jest
+      .spyOn(HttpClient.prototype, 'sendStream')
+      .mockImplementation(async (verb, requestUrl, stream) => {
         const mockMessage = new http.IncomingMessage(new net.Socket())
         mockMessage.statusCode = 200
         if (!stream.readable) {
@@ -322,50 +329,54 @@ describe('Upload Tests', () => {
         return new Promise<HttpClientResponse>(resolve => {
           resolve({
             message: mockMessage,
-            readBody: mockReadBodyEmpty
+            readBody: emptyMockReadBody
           })
-        })
-      }
-    )
-  }
-
-  function mockHttpPatch(): void {
-    // eslint-disable-next-line @typescript-eslint/unbound-method
-    HttpClient.prototype.patch = jest.fn(async (requestdata, data) => {
-      const inputData = JSON.parse(data)
-      const mockMessage = new http.IncomingMessage(new net.Socket())
-
-      // Get the name from the end of requestdata. Will be something like https://www.example.com/_apis/pipelines/workflows/15/artifacts?api-version=6.0-preview&artifactName=my-artifact
-      const artifactName = requestdata.split('=')[2]
-      let mockReadBody = mockReadBodyEmpty
-      if (inputData.Size < 1) {
-        mockMessage.statusCode = 400
-      } else if (artifactName === 'non-existent-artifact') {
-        mockMessage.statusCode = 404
-      } else {
-        mockMessage.statusCode = 200
-        const response: PatchArtifactSizeSuccessResponse = {
-          containerId: 13,
-          size: inputData.Size,
-          signedContent: 'false',
-          type: 'actions_storage',
-          name: artifactName,
-          url: `${getRuntimeUrl()}_apis/pipelines/1/runs/1/artifacts?artifactName=${artifactName}`,
-          uploadUrl: `${getRuntimeUrl()}_apis/resources/Containers/13`
-        }
-        const returnData: string = JSON.stringify(response, null, 2)
-        mockReadBody = async function(): Promise<string> {
-          return new Promise(resolve => {
-            resolve(returnData)
-          })
-        }
-      }
-      return new Promise<HttpClientResponse>(resolve => {
-        resolve({
-          message: mockMessage,
-          readBody: mockReadBody
         })
       })
-    })
+
+    /**
+     * Mocks Patch calls that are made during Artifact Association tests
+     *
+     * Simulates success and non-success status codes depending on the input size along with an appropriate
+     * payload that represents an expected response
+     */
+    jest
+      .spyOn(HttpClient.prototype, 'patch')
+      .mockImplementation(async (requestdata, data) => {
+        const inputData = JSON.parse(data)
+        const mockMessage = new http.IncomingMessage(new net.Socket())
+
+        // Get the name from the end of requestdata. Will be something like https://www.example.com/_apis/pipelines/workflows/15/artifacts?api-version=6.0-preview&artifactName=my-artifact
+        const artifactName = requestdata.split('=')[2]
+        let mockReadBody = emptyMockReadBody
+        if (inputData.Size < 1) {
+          mockMessage.statusCode = 400
+        } else if (artifactName === 'non-existent-artifact') {
+          mockMessage.statusCode = 404
+        } else {
+          mockMessage.statusCode = 200
+          const response: PatchArtifactSizeSuccessResponse = {
+            containerId: 13,
+            size: inputData.Size,
+            signedContent: 'false',
+            type: 'actions_storage',
+            name: artifactName,
+            url: `${getRuntimeUrl()}_apis/pipelines/1/runs/1/artifacts?artifactName=${artifactName}`,
+            uploadUrl: `${getRuntimeUrl()}_apis/resources/Containers/13`
+          }
+          const returnData: string = JSON.stringify(response, null, 2)
+          mockReadBody = async function(): Promise<string> {
+            return new Promise(resolve => {
+              resolve(returnData)
+            })
+          }
+        }
+        return new Promise<HttpClientResponse>(resolve => {
+          resolve({
+            message: mockMessage,
+            readBody: mockReadBody
+          })
+        })
+      })
   }
 })
