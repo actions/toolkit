d6d629ecbdee9c47b98c620c3ca5024aa1fe5f42
Bryan MacFarlane <bryanmacfarlane@github.com>
2020-04-08 10:07:08 -0400

    version filter

--- packages/tool-cache/__tests__/manifest.test.ts
+++ packages/tool-cache/__tests__/manifest.test.ts
@@ -1,9 +1,11 @@
 import * as tc from '../src/tool-cache'
-import * as mm from '../src/manifest'
+import * as mm from '../src/manifest' // --> OFF
 
 // needs to be require for core node modules to be mocked
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 import osm = require('os')
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+import cp = require('child_process')
 
 // we fetch the manifest file from master of a repo
 const versionsUrl =
@@ -19,6 +21,8 @@ describe('@actions/tool-cache-manifest', () => {
   let getSpy: jest.SpyInstance
   let platSpy: jest.SpyInstance
   let archSpy: jest.SpyInstance
+  let execSpy: jest.SpyInstance
+  let readLsbSpy: jest.SpyInstance
 
   beforeEach(() => {
     // node
@@ -28,6 +32,9 @@ describe('@actions/tool-cache-manifest', () => {
     platSpy.mockImplementation(() => os.platform)
     archSpy = jest.spyOn(osm, 'arch')
     archSpy.mockImplementation(() => os.arch)
+
+    execSpy = jest.spyOn(cp, 'execSync')
+    readLsbSpy = jest.spyOn(mm, '_readLinuxVersionFile')
 
     getSpy = jest.spyOn(tc, 'getManifestFromUrl')
     getSpy.mockImplementation(() => <mm.IToolRelease[]>manifestData)
@@ -95,5 +102,143 @@ describe('@actions/tool-cache-manifest', () => {
       'https://github.com/actions/sometool/releases/tag/1.2.3-20200402.6/sometool-1.2.3-linux-x64.tar.gz'
     )
     expect(file?.filename).toBe('sometool-1.2.3-linux-x64.tar.gz')
+  })
+
+  it('can match with linux platform version spec', async () => {
+    os.platform = 'linux'
+    os.arch = 'x64'
+
+    readLsbSpy.mockImplementation(() => {
+      return `DISTRIB_ID=Ubuntu
+        DISTRIB_RELEASE=18.04
+        DISTRIB_CODENAME=bionic
+        DISTRIB_DESCRIPTION=Ubuntu 18.04.4 LTS`
+    })
+
+    const manifest: mm.IToolRelease[] = await tc.getManifestFromUrl(versionsUrl)
+    const release: tc.IToolRelease | undefined = await tc.findFromManifest(
+      '1.2.4',
+      true,
+      manifest
+    )
+    expect(release).toBeDefined()
+    expect(release?.version).toBe('1.2.4')
+    expect(release?.files.length).toBe(1)
+    const file = release?.files[0]
+    expect(file).toBeDefined()
+    expect(file?.arch).toBe('x64')
+    expect(file?.platform).toBe('linux')
+    expect(file?.download_url).toBe(
+      'https://github.com/actions/sometool/releases/tag/1.2.4-20200402.6/sometool-1.2.4-ubuntu1804-x64.tar.gz'
+    )
+    expect(file?.filename).toBe('sometool-1.2.4-ubuntu1804-x64.tar.gz')
+  })
+
+  it('can match with darwin platform version spec', async () => {
+    os.platform = 'darwin'
+    os.arch = 'x64'
+
+    execSpy.mockImplementation(() => '10.15.1')
+
+    const manifest: mm.IToolRelease[] = await tc.getManifestFromUrl(versionsUrl)
+    const release: tc.IToolRelease | undefined = await tc.findFromManifest(
+      '1.2.4',
+      true,
+      manifest
+    )
+    expect(release).toBeDefined()
+    expect(release?.version).toBe('1.2.4')
+    expect(release?.files.length).toBe(1)
+    const file = release?.files[0]
+    expect(file).toBeDefined()
+    expect(file?.arch).toBe('x64')
+    expect(file?.platform).toBe('darwin')
+    expect(file?.download_url).toBe(
+      'https://github.com/actions/sometool/releases/tag/1.2.4-20200402.6/sometool-1.2.4-darwin1015-x64.tar.gz'
+    )
+    expect(file?.filename).toBe('sometool-1.2.4-darwin1015-x64.tar.gz')
+  })
+
+  it('does not match with unmatched linux platform version spec', async () => {
+    os.platform = 'linux'
+    os.arch = 'x64'
+
+    readLsbSpy.mockImplementation(() => {
+      return `DISTRIB_ID=Ubuntu
+        DISTRIB_RELEASE=16.04
+        DISTRIB_CODENAME=xenial
+        DISTRIB_DESCRIPTION=Ubuntu 16.04.4 LTS`
+    })
+
+    const manifest: mm.IToolRelease[] = await tc.getManifestFromUrl(versionsUrl)
+    const release: tc.IToolRelease | undefined = await tc.findFromManifest(
+      '1.2.4',
+      true,
+      manifest
+    )
+    expect(release).toBeUndefined()
+  })
+
+  it('does not match with unmatched darwin platform version spec', async () => {
+    os.platform = 'darwin'
+    os.arch = 'x64'
+
+    execSpy.mockImplementation(() => '10.14.6')
+
+    const manifest: mm.IToolRelease[] = await tc.getManifestFromUrl(versionsUrl)
+    const release: tc.IToolRelease | undefined = await tc.findFromManifest(
+      '1.2.4',
+      true,
+      manifest
+    )
+    expect(release).toBeUndefined()
+  })
+
+  it('can get version from lsb on ubuntu-18.04', async () => {
+    os.platform = 'linux'
+    os.arch = 'x64'
+
+    //existsSpy.mockImplementation(() => true)
+    readLsbSpy.mockImplementation(() => {
+      return `DISTRIB_ID=Ubuntu
+        DISTRIB_RELEASE=18.04
+        DISTRIB_CODENAME=bionic
+        DISTRIB_DESCRIPTION=Ubuntu 18.04.4 LTS`
+    })
+
+    const version = mm._getOsVersion()
+
+    expect(osm.platform()).toBe('linux')
+    expect(version).toBe('18.04')
+  })
+
+  it('can get version from lsb on ubuntu-16.04', async () => {
+    os.platform = 'linux'
+    os.arch = 'x64'
+
+    readLsbSpy.mockImplementation(() => {
+      return `DISTRIB_ID=Ubuntu
+        DISTRIB_RELEASE=16.04
+        DISTRIB_CODENAME=xenial
+        DISTRIB_DESCRIPTION="Ubuntu 16.04.6 LTS"`
+    })
+
+    const version = mm._getOsVersion()
+
+    expect(osm.platform()).toBe('linux')
+    expect(version).toBe('16.04')
+  })
+
+  // sw_vers -productVersion
+  it('can get version on macOS', async () => {
+    os.platform = 'darwin'
+    os.arch = 'x64'
+
+    execSpy.mockImplementation(() => '10.14.6')
+
+    const version = mm._getOsVersion()
+
+    expect(osm.platform()).toBe('darwin')
+    expect(version).toBe('10.14.6')
   })
 })
--- packages/tool-cache/src/manifest.ts
+++ packages/tool-cache/src/manifest.ts
@@ -2,12 +2,17 @@ import * as semver from 'semver'
 import {debug} from '@actions/core'
 
 // needs to be require for core node modules to be mocked
-// eslint-disable-next-line @typescript-eslint/no-require-imports
+/* eslint @typescript-eslint/no-require-imports: 0 */
+
 import os = require('os')
+import cp = require('child_process')
+import fs = require('fs')
 
 /*
 NOTE: versions must be sorted descending by version in the manifest
       this library short circuits on first semver spec match
+
+      platform_version is an optional filter and can be a semver spec or range
 [
   {
     "version": "1.2.3",
@@ -18,6 +23,7 @@ NOTE: versions must be sorted descending by version in the manifest
         "filename": "sometool-1.2.3-linux-x64.zip",
         "arch": "x64",
         "platform": "linux",
+        "platform_version": "18.04"
         "download_url": "https://github.com/actions/sometool/releases/tag/1.2.3-20200402.6/sometool-1.2.3-linux-x64.zip"
       },
     ...
@@ -31,9 +37,12 @@ export interface IToolReleaseFile {
   filename: string
   // 'aix', 'darwin', 'freebsd', 'linux', 'openbsd',
   // 'sunos', and 'win32'
-  // in some images built for hosted it can also be suffixed by version
-  // e.g. linux-1804
+  // platform_version is an optional semver filter
+  // TODO: do we need distribution (e.g. ubuntu).
+  //       not adding yet but might need someday.
+  //       right now, 16.04 and 18.04 work
   platform: string
+  platform_version?: string
 
   // 'arm', 'arm64', 'ia32', 'mips', 'mipsel',
   // 'ppc', 'ppc64', 's390', 's390x',
@@ -61,7 +70,7 @@ export async function _findMatch(
   let result: IToolRelease | undefined
   let match: IToolRelease | undefined
 
-  let files: IToolReleaseFile | undefined
+  let file: IToolReleaseFile | undefined
   for (const candidate of candidates) {
     const version = candidate.version
 
@@ -70,15 +79,26 @@ export async function _findMatch(
       semver.satisfies(version, versionSpec) &&
       (!stable || candidate.stable === stable)
     ) {
-      files = candidate.files.find(file => {
+      file = candidate.files.find(item => {
         debug(
-          `${file.arch}===${archFilter} && ${file.platform}===${platFilter}`
+          `${item.arch}===${archFilter} && ${item.platform}===${platFilter}`
         )
-        // TODO: handle platform-MMmm format
-        return file.arch === archFilter && file.platform === platFilter
+
+        let chk = item.arch === archFilter && item.platform === platFilter
+        if (chk && item.platform_version) {
+          const osVersion = module.exports._getOsVersion()
+
+          if (osVersion === item.platform_version) {
+            chk = true
+          } else {
+            chk = semver.satisfies(osVersion, item.platform_version)
+          }
+        }
+
+        return chk
       })
 
-      if (files) {
+      if (file) {
         debug(`matched ${candidate.version}`)
         match = candidate
         break
@@ -86,11 +106,53 @@ export async function _findMatch(
     }
   }
 
-  if (match && files) {
+  if (match && file) {
     // clone since we're mutating the file list to be only the file that matches
     result = Object.assign({}, match)
-    result.files = [files]
+    result.files = [file]
   }
 
   return result
+}
+
+export function _getOsVersion(): string {
+  // TODO: add windows and other linux, arm variants
+  // right now filtering on version is only an ubuntu and macos scenario for tools we build for hosted (python)
+  const plat = os.platform()
+  let version = ''
+
+  if (plat === 'darwin') {
+    version = cp.execSync('sw_vers -productVersion').toString()
+  } else if (plat === 'linux') {
+    // lsb_release process not in some containers, readfile
+    // Run cat /etc/lsb-release
+    // DISTRIB_ID=Ubuntu
+    // DISTRIB_RELEASE=18.04
+    // DISTRIB_CODENAME=bionic
+    // DISTRIB_DESCRIPTION="Ubuntu 18.04.4 LTS"
+    const lsbContents = module.exports._readLinuxVersionFile()
+    if (lsbContents) {
+      const lines = lsbContents.split('\n')
+      for (const line of lines) {
+        const parts = line.split('=')
+        if (parts.length === 2 && parts[0].trim() === 'DISTRIB_RELEASE') {
+          version = parts[1].trim()
+          break
+        }
+      }
+    }
+  }
+
+  return version
+}
+
+export function _readLinuxVersionFile(): string {
+  const lsbFile = '/etc/lsb-release'
+  let contents = ''
+
+  if (fs.existsSync(lsbFile)) {
+    contents = fs.readFileSync(lsbFile).toString()
+  }
+
+  return contents
 }
