19e278b80fdd0a14c188d68adcaad19bf095c15e
svartalf <self@svartalf.info>
2020-04-20 07:59:50 +0300

    Pass -o flag to the tool-cache `unzip` call.
    Without it `unzip` shows prompt if any of extracted files already exists in the destination path

--- packages/tool-cache/src/tool-cache.ts
+++ packages/tool-cache/src/tool-cache.ts
@@ -295,7 +295,7 @@ async function extractZipWin(file: string, dest: string): Promise<void> {
 
 async function extractZipNix(file: string, dest: string): Promise<void> {
   const unzipPath = await io.which('unzip')
-  await exec(`"${unzipPath}"`, [file], {cwd: dest})
+  await exec(`"${unzipPath}"`, ['-o', file], {cwd: dest})
 }
 
 /**
