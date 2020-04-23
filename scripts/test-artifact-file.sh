f8adc8d4de90953bcaf64b6d76eb097fe040c5ac
Konrad Pabjan <kopabjan@microsoft.com>
2020-04-23 20:07:42 +0200

    Update bash script

--- scripts/test-artifact-file.sh
+++ scripts/test-artifact-file.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-path=$1
-content=$2
+path="$1"
+expectedContent="$2"
 
 if [ "$path" == "" ]; then
     echo "File path not provided"
     exit 1
 fi
 
-if [ "$content" == "" ]; then
+if [ "$expectedContent" == "" ]; then
     echo "Expected file contents not provided"
     exit 1
 fi
@@ -18,8 +18,8 @@ if [ ! -f "$path" ]; then
     exit 1
 fi
 
-fileContent=$(cat $path)
-if [ "$content" != "$fileContent" ];then
-    echo "File contents are not correct, expected $content, recieved $fileContent"
+actualContent=$(cat $path)
+if [ "$actualContent" != "$expectedContent" ];then
+    echo "File contents are not correct, expected $expectedContent, recieved $actualContent"
     exit 1
 fi
