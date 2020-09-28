20dcada5f2a76eb8bb1a127f366d1ca319e52cf6
Thomas Boop <thboop@github.com>
2020-09-28 16:54:11 -0400

    swap to require

--- packages/cache/__tests__/__fixtures__/index.js
+++ packages/cache/__tests__/__fixtures__/index.js
@@ -1,7 +1,7 @@
 // Certain env variables are not set by default in a shell context and are only available in a node context from a running action
 // In order to be able to restore and save cache e2e in a shell when running CI tests, we need these env variables set
-import * as fs from 'fs'
-import * as os from 'os'
+const fs = require('fs');
+const os = require('os');
 const filePath = process.env[`GITHUB_ENV`]
 fs.appendFileSync(filePath, `ACTIONS_RUNTIME_URL=${process.env.ACTIONS_RUNTIME_URL}${os.EOL}`, {
     encoding: 'utf8'
