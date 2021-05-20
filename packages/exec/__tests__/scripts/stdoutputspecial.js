b23f9a5e2ff3dd1824af03e2018255f347ee4063
Luke Tomlinson <luketomlinson@github.com>
2021-05-20 16:05:14 -0400

    Update how split byte are sent for tests

--- packages/exec/__tests__/scripts/stdoutputspecial.js
+++ packages/exec/__tests__/scripts/stdoutputspecial.js
@@ -1,4 +1,5 @@
-process.stdout.write(Buffer.from([0xC2])) //first half of © character
-process.stdout.emit('drain') //force first byte to be sent and not appended with next one
-process.stdout.write(Buffer.from([0xA9])) //second half of © character
-
+//first half of © character
+process.stdout.write(Buffer.from([0xC2]), (err) => {
+    //write in the callback so that the second byte is sent separately
+    process.stdout.write(Buffer.from([0xA9])) //second half of © character
+})
