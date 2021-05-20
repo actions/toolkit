9bcafc4f7b0a3008ee97985d0fe253a3feb14b2b
Thomas Boop <thboop@github.com>
2021-05-20 15:48:22 -0400

    update timeouts test

--- packages/cache/__tests__/downloadUtils.test.ts
+++ packages/cache/__tests__/downloadUtils.test.ts
@@ -112,7 +112,7 @@ test('display timer works correctly', () => {
     progress.stopDisplayTimer()
     progress.setReceivedBytes(1000)
 
-    setTimeout(() => test2(), 100)
+    setTimeout(() => test2(), 500)
   }
 
   // Start the timer, update the received bytes, and call test1.
@@ -122,7 +122,7 @@ test('display timer works correctly', () => {
 
     progress.setReceivedBytes(500)
 
-    setTimeout(() => test1(), 100)
+    setTimeout(() => test1(), 500)
   }
 
   start()
