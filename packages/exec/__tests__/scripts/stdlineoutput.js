089185db68e5dce2ec6fa099c04ea23d3dd5a60c
Luke Tomlinson <luketomlinson@github.com>
2021-06-02 17:25:53 -0400

    Update stdlineoutput.js

--- packages/exec/__tests__/scripts/stdlineoutput.js
+++ packages/exec/__tests__/scripts/stdlineoutput.js
@@ -1,3 +1,5 @@
 //Default highWaterMark for readable stream buffers us 64K (2^16)
 //so we go over that to get more than a buffer's worth
-process.stdout.write('a'.repeat(2**16 + 1) + '\n');
+import * as os from 'os'
+
+process.stdout.write('a'.repeat(2**16 + 1) + os.EOL);
