7e6460102132c1b0142287bc5a00235f67be6873
Thomas Boop <thboop@github.com>
2021-06-04 01:15:00 -0400

    split out tests

--- packages/glob/__tests__/hash-files.test.ts
+++ packages/glob/__tests__/hash-files.test.ts
@@ -30,7 +30,7 @@ describe('globber', () => {
     expect(hash).toEqual('')
   })
 
-  it('correctly follows followSymbolicLinks being set', async () => {
+  it('followSymbolicLinks defaults to true', async () => {
     // Create the following layout:
     //   <root>
     //   <root>/folder-a
@@ -47,18 +47,56 @@ describe('globber', () => {
       path.join(root, 'symDir')
     )
     const testPath = path.join(root, 'symdir', '*')
-    const hashNoOptions = await hashFiles(testPath)
-    const hashSymbolicFalse = await hashFiles(testPath, {
-      followSymbolicLinks: false
-    })
-    const hashSymbolicTrue = await hashFiles(testPath, {
-      followSymbolicLinks: true
-    })
-    expect(hashNoOptions).toEqual(
+    const hash = await hashFiles(testPath)
+    expect(hash).toEqual(
       'd8a411e8f8643821bed189e627ff57151918aa554c00c10b31c693ab2dded273'
     )
-    expect(hashSymbolicFalse).toEqual('')
-    expect(hashNoOptions).toEqual(hashSymbolicTrue)
+  })
+
+  it('followSymbolicLinks set to true', async () => {
+    // Create the following layout:
+    //   <root>
+    //   <root>/folder-a
+    //   <root>/folder-a/file
+    //   <root>/symDir -> <root>/folder-a
+    const root = path.join(
+      getTestTemp(),
+      'set-to-true'
+    )
+    await fs.mkdir(path.join(root, 'realdir'), {recursive: true})
+    await fs.writeFile(path.join(root, 'realdir', 'file'), 'test file content')
+    await createSymlinkDir(
+      path.join(root, 'realdir'),
+      path.join(root, 'symDir')
+    )
+    const testPath = path.join(root, 'symdir', '*')
+    const hash = await hashFiles(testPath, {followSymbolicLinks: true})
+    expect(hash).toEqual(
+      'd8a411e8f8643821bed189e627ff57151918aa554c00c10b31c693ab2dded273'
+    )
+  })
+
+  it('followSymbolicLinks set to false', async () => {
+    // Create the following layout:
+    //   <root>
+    //   <root>/folder-a
+    //   <root>/folder-a/file
+    //   <root>/symDir -> <root>/folder-a
+    const root = path.join(
+      getTestTemp(),
+      'set-to-false'
+    )
+    await fs.mkdir(path.join(root, 'realdir'), {recursive: true})
+    await fs.writeFile(path.join(root, 'realdir', 'file'), 'test file content')
+    await createSymlinkDir(
+      path.join(root, 'realdir'),
+      path.join(root, 'symDir')
+    )
+    const testPath = path.join(root, 'symdir', '*')
+    const hash = await hashFiles(testPath, {followSymbolicLinks: false})
+    expect(hash).toEqual(
+      ''
+    )
   })
 })
 
