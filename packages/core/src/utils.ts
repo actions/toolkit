fe097332a754bdcdbcb77f3ef93570f444c8821b
Luke Tomlinson <luketomlinson@github.com>
2021-06-29 14:29:03 -0400

    Add support for notice annotation and additional properties

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -1,6 +1,9 @@
 // We use any as a valid input type
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+import {AnnotationProperties} from './core'
+import {CommandProperties} from './command'
+
 /**
  * Sanitizes an input into a string so it can be passed into issueCommand safely
  * @param input input to sanitize into a string
@@ -12,4 +15,26 @@ export function toCommandValue(input: any): string {
     return input as string
   }
   return JSON.stringify(input)
+}
+
+/**
+ *
+ * @param annotationProperties
+ * @returns The command properties to send with the actual annotaiton command
+ * See: https://github.com/actions/runner/blob/ee34f4842e747b452e13235836c92b2bb1606816/src/Runner.Worker/ActionCommandManager.cs#L566
+ */
+export function toCommandProperties(
+  annotationProperties: AnnotationProperties
+): CommandProperties {
+  if (!Object.keys(annotationProperties).length) {
+    return {}
+  }
+
+  return {
+    title: annotationProperties.title,
+    line: annotationProperties.startLine,
+    end_line: annotationProperties.endLine,
+    col: annotationProperties.startColumn,
+    end_column: annotationProperties.endColumn
+  }
 }
