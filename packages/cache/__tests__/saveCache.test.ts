35ad4914959373d46e4e7841203cd9605ad36af3
aparna-ravindra <aparna-ravindra@github.com>
2021-10-07 21:21:06 +0530

    increasing cache limit size to 10gb

--- packages/cache/__tests__/saveCache.test.ts
+++ packages/cache/__tests__/saveCache.test.ts
@@ -46,7 +46,7 @@ test('save with large cache outputs should fail', async () => {
 
   const createTarMock = jest.spyOn(tar, 'createTar')
 
-  const cacheSize = 6 * 1024 * 1024 * 1024 //~6GB, over the 5GB limit
+  const cacheSize = 11 * 1024 * 1024 * 1024 //~11GB, over the 10GB limit
   jest
     .spyOn(cacheUtils, 'getArchiveFileSizeInBytes')
     .mockReturnValueOnce(cacheSize)
@@ -56,7 +56,7 @@ test('save with large cache outputs should fail', async () => {
     .mockReturnValueOnce(Promise.resolve(compression))
 
   await expect(saveCache([filePath], primaryKey)).rejects.toThrowError(
-    'Cache size of ~6144 MB (6442450944 B) is over the 5GB limit, not saving cache.'
+    'Cache size of ~11264 MB (11811160064 B) is over the 10GB limit, not saving cache.'
   )
 
   const archiveFolder = '/foo/bar'
--- packages/cache/src/cache.ts
+++ packages/cache/src/cache.ts
@@ -179,7 +179,7 @@ export async function saveCache(
       throw new Error(
         `Cache size of ~${Math.round(
           archiveFileSize / (1024 * 1024)
-        )} MB (${archiveFileSize} B) is over the 5GB limit, not saving cache.`
+        )} MB (${archiveFileSize} B) is over the 10GB limit, not saving cache.`
       )
     }
 
