3c38dcfe1f190354bbf7c204eddb3b151f06cbb9
Shubham Tiwari <tiwarishub@github.com>
2022-03-23 02:40:43 +0530

    added ACTIONS_CACHE_URL in fixtures

--- packages/cache/__tests__/__fixtures__/index.js
+++ packages/cache/__tests__/__fixtures__/index.js
@@ -9,6 +9,9 @@ fs.appendFileSync(filePath, `ACTIONS_RUNTIME_URL=${process.env.ACTIONS_RUNTIME_U
 fs.appendFileSync(filePath, `ACTIONS_RUNTIME_TOKEN=${process.env.ACTIONS_RUNTIME_TOKEN}${os.EOL}`, {
     encoding: 'utf8'
 })
+fs.appendFileSync(filePath, `ACTIONS_CACHE_URL=${process.env.ACTIONS_CACHE_URL}${os.EOL}`, {
+    encoding: 'utf8'
+})
 fs.appendFileSync(filePath, `GITHUB_RUN_ID=${process.env.GITHUB_RUN_ID}${os.EOL}`, {
     encoding: 'utf8'
 })
