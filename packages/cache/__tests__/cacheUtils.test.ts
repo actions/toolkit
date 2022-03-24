a4539ea36c53b951f7878ce9a2f35b5fec0e58c4
Shubham Tiwari <tiwarishub@github.com>
2022-03-24 11:55:57 +0530

    lint

--- packages/cache/__tests__/cacheUtils.test.ts
+++ packages/cache/__tests__/cacheUtils.test.ts
@@ -35,14 +35,13 @@ test('assertDefined returns value', () => {
 
 test('isFeatureAvailable returns true if server url is set', () => {
   try {
-      process.env['ACTIONS_CACHE_URL'] = 'http://cache.com'
-      expect(cacheUtils.isFeatureAvailable()).toBe(true)
+    process.env['ACTIONS_CACHE_URL'] = 'http://cache.com'
+    expect(cacheUtils.isFeatureAvailable()).toBe(true)
   } finally {
-     delete process.env['ACTIONS_CACHE_URL']
+    delete process.env['ACTIONS_CACHE_URL']
   }
 })
 
 test('isFeatureAvailable returns false if server url is not set', () => {
   expect(cacheUtils.isFeatureAvailable()).toBe(false)
 })
-
