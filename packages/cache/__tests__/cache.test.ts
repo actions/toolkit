2a53a08370443a0637b860dc921390d7b7939720
Shubham Tiwari <tiwarishub@github.com>
2022-03-25 00:26:40 +0530

    linting

--- packages/cache/__tests__/cache.test.ts
+++ packages/cache/__tests__/cache.test.ts
@@ -4,10 +4,10 @@ test('isFeatureAvailable returns true if server url is set', () => {
   try {
       process.env['ACTIONS_CACHE_URL'] = 'http://cache.com'
       expect(cache.isFeatureAvailable()).toBe(true)
-    } finally {
+  } finally {
       delete process.env['ACTIONS_CACHE_URL']
-    }
-  })
+  }
+})
 
 test('isFeatureAvailable returns false if server url is not set', () => {
   expect(cache.isFeatureAvailable()).toBe(false)
