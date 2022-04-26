fccc5ee6e648101244fb9c196bbfc9b9944cec39
Rob Herley <robherley@github.com>
2022-04-26 17:31:17 +0000

    crc: move tbl out of class, more tests

--- packages/artifact/__tests__/crc64.test.ts
+++ packages/artifact/__tests__/crc64.test.ts
@@ -2,8 +2,8 @@ import CRC64 from '../src/internal/crc64'
 
 const fixtures = {
   data:
-    '🚀 👉😎👉 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  expected: 'D170FCD035F4D958'
+    '🚀 👉😎👉 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n',
+  expected: '846CE4ADAD6223ED'
 }
 
 describe('@actions/artifact/src/internal/crc64', () => {
@@ -18,5 +18,26 @@ describe('@actions/artifact/src/internal/crc64', () => {
     const buf = Buffer.from(fixtures.data)
     crc.update(buf)
     expect(crc.digest()).toEqual(fixtures.expected)
+  })
+
+  it('CRC64 from split data', async () => {
+    const crc = new CRC64()
+    const splits = fixtures.data.split('\n').slice(0, -1)
+    for (const split of splits) {
+      crc.update(`${split}\n`)
+    }
+    expect(crc.digest()).toEqual(fixtures.expected)
+  })
+
+  it('flips 64 bits', async () => {
+    const tests = [
+      [BigInt(0), BigInt('0xffffffffffffffff')],
+      [BigInt('0xffffffffffffffff'), BigInt(0)],
+      [BigInt('0xdeadbeef'), BigInt('0xffffffff21524110')]
+    ]
+
+    for (const [input, expected] of tests) {
+      expect(CRC64.flip64Bits(input)).toEqual(expected)
+    }
   })
 })
