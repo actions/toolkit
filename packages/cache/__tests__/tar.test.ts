845770f824f64c17ce307d0123f59b15f068a714
Sankalp Kotewar <98868223+kotewar@users.noreply.github.com>
2022-06-06 07:56:13 +0000

    Revert "Revert "Changed an import in test case to standard import""
    This reverts commit c1bb3fb67983258350acda4c9fdd2a56a7992d1b.

--- packages/cache/__tests__/tar.test.ts
+++ packages/cache/__tests__/tar.test.ts
@@ -5,7 +5,7 @@ import {CacheFilename, CompressionMethod} from '../src/internal/constants'
 import * as tar from '../src/internal/tar'
 import * as utils from '../src/internal/cacheUtils'
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-import fs = require('fs')
+import * as fs from 'fs'
 
 jest.mock('@actions/exec')
 jest.mock('@actions/io')
