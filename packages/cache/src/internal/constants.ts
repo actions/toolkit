14d8f65f100ea291360d89239ba395c1dd32265b
Lovepreet Singh <pdotl@github.com>
2022-08-18 11:10:41 +0000

    Remove unused constants

--- packages/cache/src/internal/constants.ts
+++ packages/cache/src/internal/constants.ts
@@ -11,12 +11,6 @@ export enum CompressionMethod {
   Zstd = 'zstd'
 }
 
-export enum TarOperation {
-  Create = 'create',
-  List = 'list',
-  Extract = 'extract'
-}
-
 // The default number of retry attempts.
 export const DefaultRetryAttempts = 2
 
