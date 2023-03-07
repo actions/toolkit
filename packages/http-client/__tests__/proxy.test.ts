fe3ff532cc408537efd4ae00b1a137334c7c13a3
Ferenc Hammerl <31069338+fhammerl@users.noreply.github.com>
2023-03-07 10:01:49 +0000

    port runner proxy

--- packages/http-client/__tests__/proxy.test.ts
+++ packages/http-client/__tests__/proxy.test.ts
@@ -28,7 +28,7 @@ describe('proxy', () => {
     _clearVars()
   })
 
-  afterEach(() => {})
+  afterEach(() => { })
 
   afterAll(async () => {
     _clearVars()
@@ -196,7 +196,7 @@ describe('proxy', () => {
     expect(_proxyConnects).toEqual(['httpbin.org:80'])
   })
 
-  it('HttoClient does basic http get request when bypass proxy', async () => {
+  it('HttpClient does basic http get request when bypass proxy', async () => {
     process.env['http_proxy'] = _proxyUrl
     process.env['no_proxy'] = 'httpbin.org'
     const httpClient = new httpm.HttpClient()
@@ -237,29 +237,6 @@ describe('proxy', () => {
     expect(_proxyConnects).toHaveLength(0)
   })
 
-  it('HttpClient bypasses proxy for loopback addresses (localhost, ::1, 127.*)', async () => {
-    // setup a server listening on localhost:8091
-    const server = http.createServer(function(request, response) {
-      response.writeHead(200)
-      request.pipe(response)
-    })
-    server.listen(8091)
-    try {
-      process.env['http_proxy'] = _proxyUrl
-      const httpClient = new httpm.HttpClient()
-      const res: httpm.HttpClientResponse = await httpClient.get(
-        'http://localhost:8091'
-      )
-      expect(res.message.statusCode).toBe(200)
-      const body: string = await res.readBody()
-      expect(body).toEqual('')
-      // proxy at _proxyUrl was ignored
-      expect(_proxyConnects).toEqual([])
-    } finally {
-      server.close()
-    }
-  })
-
   it('proxyAuth not set in tunnel agent when authentication is not provided', async () => {
     process.env['https_proxy'] = 'http://127.0.0.1:8080'
     const httpClient = new httpm.HttpClient()
@@ -281,7 +258,132 @@ describe('proxy', () => {
     expect(agent.proxyOptions.port).toBe('8080')
     expect(agent.proxyOptions.proxyAuth).toBe('user:password')
   })
+
+  // unit tests from actions/runner
+  it('should prefer lowercase over uppercase ENVs', async () => {
+    process.env['http_proxy'] = 'http://127.0.0.1:7777'
+    process.env['HTTP_PROXY'] = 'http://127.0.0.1:8888'
+    process.env['https_proxy'] = 'https://127.0.0.1:8888'
+    process.env['HTTPS_PROXY'] = 'https://127.0.0.1:7777'
+    const httpClient = new httpm.HttpClient()
+
+    const httpAgent: any = httpClient.getAgent('http://some-url')
+    expect(httpAgent.proxyOptions.host).toBe('127.0.0.1')
+    expect(httpAgent.proxyOptions.port).toBe('7777')
+
+    const httpsAgent: any = httpClient.getAgent('https://some-url')
+    expect(httpsAgent.proxyOptions.host).toBe('127.0.0.1')
+    expect(httpsAgent.proxyOptions.port).toBe('8888')
+  })
+
+  it('should not set proxy on invalid input', async () => {
+    process.env['http_proxy'] = '127.0.0.1:7777'
+    process.env['https_proxy'] = '127.0.0.1:8888'
+    const httpClient = new httpm.HttpClient()
+
+    // Different from actions/runner, we throw an error here while the runner proceeds without a proxy
+    expect(() => httpClient.getAgent('http://some-url')).toThrow()
+    expect(() => httpClient.getAgent('https://some-url')).toThrow()
+  })
+
+  it('should bypass no_proxy hosts', async () => {
+    process.env['http_proxy'] = '127.0.0.1:7777'
+    process.env['https_proxy'] = '127.0.0.1:8888'
+    process.env['no_proxy'] = 'github.com, .google.com, example.com:444, 192.168.0.123:123, 192.168.1.123'
+
+    expect(pm.checkBypass(new URL('https://actions.com'))).toBeFalsy();
+    expect(pm.checkBypass(new URL('https://ggithub.com'))).toBeFalsy();
+    expect(pm.checkBypass(new URL('https://github.comm'))).toBeFalsy();
+    expect(pm.checkBypass(new URL('https://google.com'))).toBeFalsy();
+    expect(pm.checkBypass(new URL('https://example.com'))).toBeFalsy();
+    expect(pm.checkBypass(new URL('http://example.com:333'))).toBeFalsy();
+    expect(pm.checkBypass(new URL('http://192.168.0.123:123'))).toBeTruthy(); // DIFF
+    expect(pm.checkBypass(new URL('http://192.168.1.123/home'))).toBeTruthy(); // DIFF
+
+    expect(pm.checkBypass(new URL('https://github.com'))).toBeTruthy()
+    expect(pm.checkBypass(new URL('https://GITHUB.COM'))).toBeTruthy()
+    expect(pm.checkBypass(new URL('https://github.com/owner/repo'))).toBeTruthy()
+    expect(pm.checkBypass(new URL('https://actions.github.com'))).toBeTruthy()
+    expect(pm.checkBypass(new URL('https://mails.google.com'))).toBeTruthy()
+    expect(pm.checkBypass(new URL('https://MAILS.GOOGLE.com'))).toBeTruthy()
+    expect(pm.checkBypass(new URL('https://mails.v2.google.com'))).toBeTruthy()
+    expect(pm.checkBypass(new URL('http://mails.v2.v3.google.com/inbox'))).toBeTruthy()
+    expect(pm.checkBypass(new URL('https://example.com:444'))).toBeTruthy()
+    expect(pm.checkBypass(new URL('http://example.com:444'))).toBeTruthy()
+    expect(pm.checkBypass(new URL('http://example.COM:444'))).toBeTruthy()
+  })
 })
+
+it('should not use http_proxy for https requests if https_proxy is not set', async () => {
+  process.env['http_proxy'] = 'http://127.0.0.1:7777/'
+
+  expect(pm.getProxyUrl(new URL('http://example.com'))).toBeDefined()
+  expect(pm.getProxyUrl(new URL('https://example.com'))).toBeUndefined()
+})
+
+it('HttpClient does basic https get request when bypass proxy', async () => {
+  process.env['https_proxy'] = _proxyUrl
+  process.env['no_proxy'] = 'httpbin.org'
+  const httpClient = new httpm.HttpClient()
+  const res: httpm.HttpClientResponse = await httpClient.get(
+    'https://httpbin.org/get'
+  )
+  expect(res.message.statusCode).toBe(200)
+  const body: string = await res.readBody()
+  const obj = JSON.parse(body)
+  expect(obj.url).toBe('https://httpbin.org/get')
+  expect(_proxyConnects).toHaveLength(0)
+})
+
+it('HttpClient bypasses proxy for loopback addresses (localhost, ::1, 127.*)', async () => {
+  // setup a server listening on localhost:8091
+  var server = http.createServer(function (request, response) {
+    response.writeHead(200);
+    request.pipe(response);
+  });
+  await server.listen(8091)
+  try {
+    process.env['http_proxy'] = _proxyUrl
+    const httpClient = new httpm.HttpClient()
+    const res: httpm.HttpClientResponse = await httpClient.get(
+      'http://localhost:8091'
+    )
+    expect(res.message.statusCode).toBe(200)
+    const body: string = await res.readBody()
+    expect(body).toEqual('');
+    // proxy at _proxyUrl was ignored
+    expect(_proxyConnects).toEqual([])
+  }
+  finally {
+    await server.close()
+  }
+})
+
+it('HttpClient does basic https get request when bypass proxy', async () => {
+  process.env['https_proxy'] = _proxyUrl
+  process.env['no_proxy'] = 'httpbin.org'
+  const httpClient = new httpm.HttpClient()
+  const res: httpm.HttpClientResponse = await httpClient.get(
+    'https://httpbin.org/get'
+  )
+  expect(res.message.statusCode).toBe(200)
+  const body: string = await res.readBody()
+  const obj = JSON.parse(body)
+  expect(obj.url).toBe('https://httpbin.org/get')
+  expect(_proxyConnects).toHaveLength(0)
+})
+
+it('should not use https_proxy for http requests if http_proxy is not set', async () => {
+  process.env['https_proxy'] = 'https://127.0.0.1:7777/'
+
+  expect(pm.getProxyUrl(new URL('http://example.com'))).toBeUndefined()
+  expect(pm.getProxyUrl(new URL('https://example.com'))).toBeDefined()
+})
+
+// it('should detect loopback ip addresses', async () => {
+//   process.env['http_proxy'] = 'http://nonlocal.faraway.com:7777/'
+//   expect(pm.getProxyUrl(new URL('http://localhost'))).toBeUndefined()
+// })
 
 function _clearVars(): void {
   delete process.env.http_proxy
