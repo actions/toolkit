7cbe5b94702ce27d2c6515a15878fa7ffba7c8f8
Cory Miller <13227161+cory-miller@users.noreply.github.com>
2023-03-13 16:39:43 +0000

    .

--- packages/io/__tests__/io.test.ts
+++ packages/io/__tests__/io.test.ts
@@ -1,6 +1,5 @@
 import * as child from 'child_process'
 import {promises as fs} from 'fs'
-import { constants } from 'fs/promises'
 import * as os from 'os'
 import * as path from 'path'
 import * as io from '../src/io'
