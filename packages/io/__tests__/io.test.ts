75cc209d80beede096d02ed7814bc700a65cdf84
Vallie Joseph <vmjoseph@github.com>
2023-03-15 15:10:46 +0000

    moving comment placement

--- packages/io/__tests__/io.test.ts
+++ packages/io/__tests__/io.test.ts
@@ -332,9 +332,6 @@ describe('rmRF', () => {
     await fs.appendFile(filePath, 'some data')
     await assertExists(filePath)
 
-    // for windows, we need to explicitly set an exclusive lock
-    // or we won't get an EBUSY error. this can be fixed after
-    // https://github.com/libuv/libuv/issues/3267 is resolved
     // exclusive lock windows flag: https://github.com/nodejs/node/blob/c2e4b1fa9ad0b744616c4e4c13a5017772a630c4/deps/uv/src/win/fs.c#L499-L513
     const fd = await fs.open(
       filePath,
@@ -342,7 +339,9 @@ describe('rmRF', () => {
     )
     if (ioUtil.IS_WINDOWS) {
       try {
-        // additionally, can't stat an open file on Windows without getting EPERM
+        // for windows, we need to explicitly set an exclusive lock
+        // or we won't get an EBUSY error. this can be fixed after
+        // https://github.com/libuv/libuv/issues/3267 is resolved
         await io.rmRF(testPath)
       } catch (err) {
         expect(err.message).toContain('EBUSY')
