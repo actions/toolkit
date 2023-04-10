48de9adf84970a654970499d49cbaae767eaad9d
Boris Staal <boris@staal.io>
2023-04-10 02:16:14 -0500

    @actions/cache-s3 implemented
    Package Cache-S3 implemented

---
+++ packages/cache-s3/src/internal/config.ts
@@ -0,0 +1,29 @@
+import {getProxyUrl} from '@actions/http-client'
+import {S3ClientConfig} from '@aws-sdk/client-s3'
+import {NodeHttpHandler} from '@aws-sdk/node-http-handler'
+import ProxyAgent from 'proxy-agent'
+
+export function getConfig(
+  region: string,
+  access_key_id: string,
+  secret_access_key: string
+): S3ClientConfig {
+  const proxy = getProxyUrl('https://amazonaws.com')
+
+  const config: S3ClientConfig = {
+    credentials: {
+      accessKeyId: access_key_id,
+      secretAccessKey: secret_access_key
+    },
+    forcePathStyle: true,
+    region
+  }
+
+  if (proxy) {
+    config.requestHandler = new NodeHttpHandler({
+      httpsAgent: ProxyAgent(proxy)
+    })
+  }
+
+  return config
+}
