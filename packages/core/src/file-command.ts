15a848a9ab93899d9bb6440d8352c8cebc7f6e06
Vallie Joseph <vmjoseph@github.com>
2023-07-31 19:40:34 +0000

    updating uuid

--- packages/core/src/file-command.ts
+++ packages/core/src/file-command.ts
@@ -5,7 +5,7 @@
 
 import * as fs from 'fs'
 import * as os from 'os'
-import {v4 as uuidv4} from 'uuid'
+import {v7 as uuidv7} from 'uuid'
 import {toCommandValue} from './utils'
 
 export function issueFileCommand(command: string, message: any): void {
@@ -25,7 +25,7 @@ export function issueFileCommand(command: string, message: any): void {
 }
 
 export function prepareKeyValueMessage(key: string, value: any): string {
-  const delimiter = `ghadelimiter_${uuidv4()}`
+  const delimiter = `ghadelimiter_${uuidv7()}`
   const convertedValue = toCommandValue(value)
 
   // These should realistically never happen, but just in case someone finds a
--- packages/tool-cache/src/tool-cache.ts
+++ packages/tool-cache/src/tool-cache.ts
@@ -10,7 +10,7 @@ import * as stream from 'stream'
 import * as util from 'util'
 import {ok} from 'assert'
 import {OutgoingHttpHeaders} from 'http'
-import uuidV4 from 'uuid/v4'
+import uuidV7 from 'uuid/v7'
 import {exec} from '@actions/exec/lib/exec'
 import {ExecOptions} from '@actions/exec/lib/interfaces'
 import {RetryHelper} from './retry-helper'
@@ -41,7 +41,7 @@ export async function downloadTool(
   auth?: string,
   headers?: OutgoingHttpHeaders
 ): Promise<string> {
-  dest = dest || path.join(_getTempDirectory(), uuidV4())
+  dest = dest || path.join(_getTempDirectory(), uuidV7())
   await io.mkdirP(path.dirname(dest))
   core.debug(`Downloading ${url}`)
   core.debug(`Destination ${dest}`)
@@ -651,7 +651,7 @@ export async function findFromManifest(
 async function _createExtractFolder(dest?: string): Promise<string> {
   if (!dest) {
     // create a temp dir
-    dest = path.join(_getTempDirectory(), uuidV4())
+    dest = path.join(_getTempDirectory(), uuidV7())
   }
   await io.mkdirP(dest)
   return dest
