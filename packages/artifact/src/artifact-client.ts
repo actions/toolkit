c24983bf08c598d186ebe7b27eb3189f6082d93d
Konrad Pabjan <konradpabjan@github.com>
2023-08-01 10:02:46 -0400

    Preparation for v4

--- packages/artifact/src/artifact-client.ts
+++ packages/artifact/src/artifact-client.ts
@@ -1,15 +1,7 @@
-import {UploadOptions} from './internal/upload-options'
-import {UploadResponse} from './internal/upload-response'
-import {DownloadOptions} from './internal/download-options'
-import {DownloadResponse} from './internal/download-response'
 import {ArtifactClient, DefaultArtifactClient} from './internal/artifact-client'
 
 export {
   ArtifactClient,
-  UploadResponse,
-  UploadOptions,
-  DownloadResponse,
-  DownloadOptions
 }
 
 /**
