02d079213fff2efe0dd06128c73c383b366748a1
Konrad Pabjan <konradpabjan@github.com>
2023-08-03 19:33:38 -0400

    Artifact name validation + zip specification creation

--- packages/artifact/src/internal/upload/upload-artifact.ts
+++ packages/artifact/src/internal/upload/upload-artifact.ts
@@ -1,17 +1,39 @@
+import * as core from '@actions/core'
 import {UploadOptions} from './upload-options'
 import {UploadResponse} from './upload-response'
+import {validateArtifactName} from './path-and-artifact-name-validation'
+import {
+  UploadZipSpecification,
+  getUploadZipSpecification,
+  validateRootDirectory
+} from './upload-zip-specification'
 
 export async function uploadArtifact(
   name: string,
-  files: string[], // eslint-disable-line @typescript-eslint/no-unused-vars
-  rootDirectory: string, // eslint-disable-line @typescript-eslint/no-unused-vars
+  files: string[],
+  rootDirectory: string,
   options?: UploadOptions | undefined // eslint-disable-line @typescript-eslint/no-unused-vars
 ): Promise<UploadResponse> {
+  validateArtifactName(name)
+  validateRootDirectory(rootDirectory)
+
+  const zipSpecification: UploadZipSpecification[] = getUploadZipSpecification(
+    files,
+    rootDirectory
+  )
+  if (zipSpecification.length === 0) {
+    core.warning(`No files were found to upload`)
+    return {
+      success: false
+    }
+  }
+
   // TODO - Implement upload functionality
 
   const uploadResponse: UploadResponse = {
-    artifactName: name,
-    size: 0
+    success: true,
+    size: 0,
+    id: 0
   }
 
   return uploadResponse
