8c1e6a00f05a4d09676361f2096a8e48191571e5
Tatyana Kostromskaya <32135588+takost@users.noreply.github.com>
2023-09-15 13:28:29 +0000

    try to fix test

--- packages/github/__tests__/github.test.ts
+++ packages/github/__tests__/github.test.ts
@@ -1,5 +1,5 @@
 import * as http from 'http'
-import proxy from 'proxy'
+import * as proxy from 'proxy'
 import {getOctokit} from '../src/github'
 import {GitHub, getOctokitOptions} from '../src/utils'
 
@@ -12,7 +12,7 @@ describe('@actions/github', () => {
 
   beforeAll(async () => {
     // Start proxy server
-    proxyServer = proxy()
+    proxyServer = proxy.createProxy()
     await new Promise(resolve => {
       const port = Number(proxyUrl.split(':')[2])
       proxyServer.listen(port, () => resolve(null))
