a1badb1a94245b0df7f7bf47b3027ece2c5f100a
Dilum Aluthge <dilum@aluthge.com>
2024-01-05 15:29:03 -0500

    More stuff

--- packages/io/src/io.ts
+++ packages/io/src/io.ts
@@ -6,14 +6,14 @@ import * as ioUtil from './io-util'
  * Interface for cp/mv options
  */
 export interface CopyOptions {
-  /** Optional. Whether to recursively copy all subdirectories. Defaults to false */
-  recursive?: boolean
-  /** Optional. Whether to overwrite existing files in the destination. Defaults to true */
-  force?: boolean
-  /** Optional. Whether to copy the source directory along with all the files. Only takes effect when recursive=true and copying a directory. Default is true*/
-  copySourceDirectory?: boolean
-  /** Optional. Whether to preserve timestamps when copying. Default is false */
-  preserveTimestamps?: boolean
+    /** Optional. Whether to recursively copy all subdirectories. Defaults to false */
+    recursive?: boolean
+    /** Optional. Whether to overwrite existing files in the destination. Defaults to true */
+    force?: boolean
+    /** Optional. Whether to copy the source directory along with all the files. Only takes effect when recursive=true and copying a directory. Default is true*/
+    copySourceDirectory?: boolean
+    /** Optional. Whether to preserve timestamps when copying. Default is false */
+    preserveTimestamps?: boolean
 }
 
 /**
