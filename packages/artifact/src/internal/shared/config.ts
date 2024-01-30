3b02a6fdc5624c115b50a5e41d9f596e4a7a3f60
eggyhead <28715808+eggyhead@users.noreply.github.com>
2024-01-30 21:55:50 +0000

    updating alowed hosts in isGhes check
    updating alowed hosts in artifact ghes check
    
    using dot prepend ghe host

--- packages/artifact/src/internal/shared/config.ts
+++ packages/artifact/src/internal/shared/config.ts
@@ -27,7 +27,12 @@ export function isGhes(): boolean {
   const ghUrl = new URL(
     process.env['GITHUB_SERVER_URL'] || 'https://github.com'
   )
-  return ghUrl.hostname.toUpperCase() !== 'GITHUB.COM'
+
+  const hostname = ghUrl.hostname.trimEnd().toUpperCase()
+  const isGitHubHost = (hostname == 'GITHUB.COM')
+  const isGheHost = (hostname.endsWith('.GHE.COM') || hostname.endsWith('.GHE.LOCALHOST'))
+
+  return !isGitHubHost && !isGheHost
 }
 
 export function getGitHubWorkspaceDir(): string {
