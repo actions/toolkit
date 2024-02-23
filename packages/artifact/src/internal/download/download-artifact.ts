d3301c9bc26a357dff44590e92f2918ef7e48b31
bethanyj28 <bethanyj28@github.com>
2024-02-23 08:42:23 -0500

    update path parsing

--- packages/artifact/src/internal/download/download-artifact.ts
+++ packages/artifact/src/internal/download/download-artifact.ts
@@ -94,7 +94,8 @@ export async function streamExtractExternal(
       })
       .pipe(unzip.Parse())
       .on('entry', (entry: unzip.Entry) => {
-        const fullPath = path.normalize(path.join(directory, entry.path))
+        const entryPath = path.normalize(entry.path).replace(/^(\.\.(\/|\\|$))+/, '')
+        const fullPath = path.join(directory, entryPath)
         core.debug(`Extracting artifact entry: ${fullPath}`)
         if (entry.type === 'Directory') {
           promises.push(resolveOrCreateDirectory(fullPath).then(() => {}))
