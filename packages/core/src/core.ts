5fc21de45e857da48cc41b7b1d94991714a0fb96
Benedek Kozma <cyberbeni@gmail.com>
2024-10-16 17:11:39 +0200

    Allow passing non-async function to group

--- packages/core/src/core.ts
+++ packages/core/src/core.ts
@@ -324,7 +324,7 @@ export function endGroup(): void {
  * @param name The name of the group
  * @param fn The function to wrap in the group
  */
-export async function group<T>(name: string, fn: () => Promise<T>): Promise<T> {
+export async function group<T>(name: string, fn: () => T | Promise<T>): Promise<T> {
   startGroup(name)
 
   let result: T
