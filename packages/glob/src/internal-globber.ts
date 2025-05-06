0a7ac702c0cee63a0cae5e8d342b943f0f41d51a
Ryan Ghadimi <ghadimir@github.com>
2025-05-06 18:10:37 +0000

    log ignored files

--- packages/glob/src/internal-globber.ts
+++ packages/glob/src/internal-globber.ts
@@ -104,6 +104,7 @@ export class DefaultGlobber implements Globber {
 
     // Search
     const traversalChain: string[] = [] // used to detect cycles
+    let loggedFirstHiddenMessage: boolean = false // if printing hidden, is this the first one
     while (stack.length) {
       // Pop
       const item = stack.pop() as SearchState
@@ -130,6 +131,11 @@ export class DefaultGlobber implements Globber {
 
       // Hidden file or directory?
       if (options.excludeHiddenFiles && path.basename(item.path).match(/^\./)) {
+        if(!loggedFirstHiddenMessage) {
+          core.info(`Ignoring the following hidden files and directories`)
+          loggedFirstHiddenMessage = true
+        }
+        core.info(`- ${item.path}`)
         continue
       }
 
