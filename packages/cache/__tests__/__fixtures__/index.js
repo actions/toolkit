d50f1ac1b9e2d3ae4e5ac7f7d9467e1c2925cc55
Ryan Ghadimi <ghadimir@github.com>
2025-05-06 20:02:27 +0000

    change url

--- packages/cache/__tests__/__fixtures__/index.js
+++ packages/cache/__tests__/__fixtures__/index.js
@@ -6,12 +6,12 @@ const filePath = process.env[`GITHUB_ENV`]
 fs.appendFileSync(filePath, `ACTIONS_RUNTIME_TOKEN=${process.env.ACTIONS_RUNTIME_TOKEN}${os.EOL}`, {
     encoding: 'utf8'
 })
-fs.appendFileSync(filePath, `ACTIONS_CACHE_URL=${process.env.ACTIONS_CACHE_URL}${os.EOL}`, {
+fs.appendFileSync(filePath, `ACTIONS_CACHE_URL=${process.env.ACTIONS_RESULTS_URL}${os.EOL}`, {
     encoding: 'utf8'
 })
 fs.appendFileSync(filePath, `GITHUB_RUN_ID=${process.env.GITHUB_RUN_ID}${os.EOL}`, {
     encoding: 'utf8'
 })
-fs.appendFileSync(filePath, `ACTIONS_CACHE_SERVICE_V2=true`, {
+fs.appendFileSync(filePath, `ACTIONS_CACHE_SERVICE_V2=true${os.EOL}`, {
     encoding: 'utf8'
 })
