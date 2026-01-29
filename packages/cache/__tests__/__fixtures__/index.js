ea8b69c3cdb649b1b8a9d9762a8f4d0537950fef
Daniel Kennedy <danwkennedy@github.com>
2026-01-29 12:47:52 -0500

    Update the fixture to ESM syntax

--- packages/cache/__tests__/__fixtures__/index.js
+++ packages/cache/__tests__/__fixtures__/index.js
@@ -1,8 +1,9 @@
 // Certain env variables are not set by default in a shell context and are only available in a node context from a running action
 // In order to be able to restore and save cache e2e in a shell when running CI tests, we need these env variables set
-const fs = require('fs');
-const os = require('os');
-const filePath = process.env[`GITHUB_ENV`]
+import fs from 'fs'
+import os from 'os'
+
+const filePath = process.env['GITHUB_ENV']
 fs.appendFileSync(filePath, `ACTIONS_CACHE_SERVICE_V2=true${os.EOL}`, {
     encoding: 'utf8'
 })
