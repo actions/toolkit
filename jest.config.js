9d912b18401c7590566ae76d8555b6f063daa852
Daniel Kennedy <danwkennedy@github.com>
2026-01-29 11:26:14 -0500

    `@actions/tool-cache`: convert to an ESM module (#2274)
    * `@actions/tool-cache`: convert to an ESM module
    
    * Fix jest config
    
    * Downgrade `nock` since it's conflicting with `@actions/attest`'s version

--- jest.config.js
+++ jest.config.js
@@ -15,11 +15,11 @@ module.exports = {
     '^@actions/http-client/lib/interfaces$': '<rootDir>/packages/http-client/lib/interfaces.js',
     '^@actions/github$': '<rootDir>/packages/github/lib/github.js',
     '^@actions/github/lib/utils$': '<rootDir>/packages/github/lib/utils.js',
-    '^@actions/glob$': '<rootDir>/packages/glob/lib/glob.js'
+    '^@actions/glob$': '<rootDir>/packages/glob/lib/glob.js',
+    '^@actions/tool-cache$': '<rootDir>/packages/tool-cache/lib/tool-cache.js'
   },
   transform: {
     '^.+\\.(ts|js)$': ['ts-jest', {
-      isolatedModules: true,
       diagnostics: {warnOnly: true},
       tsconfig: {
         allowJs: true,
@@ -30,7 +30,7 @@ module.exports = {
     }]
   },
   transformIgnorePatterns: [
-    '/node_modules/(?!(@octokit|@actions/github|@actions/http-client|@actions/io|@actions/exec|@actions/core|@actions/glob|universal-user-agent|before-after-hook)/)'
+    '/node_modules/(?!(@octokit|@actions/github|@actions/http-client|@actions/io|@actions/exec|@actions/core|@actions/glob|@actions/tool-cache|universal-user-agent|before-after-hook)/)'
   ],
   verbose: true
 }
